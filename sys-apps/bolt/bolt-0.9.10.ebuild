zmedico/gentoo

commit 5ca8c25a6abf44d015bc807419059be4f5956b9a

sys-apps/bolt: Add some missing test deps
Closes: https://bugs.gentoo.org/932003
Signed-off-by: Patrick McLean <chutzpah@gentoo.org>

sys-apps/bolt/bolt-0.9.8-r1.ebuild
@@ -3,7 +3,8 @@
 
 EAPI=8
 
-inherit linux-info meson udev
+PYTHON_COMPAT=( python3_1{0..2} )
+inherit linux-info python-any-r1 meson udev
 
 DESCRIPTION="Userspace system daemon to enable security levels for Thunderbolt 3"
 HOMEPAGE="https://gitlab.freedesktop.org/bolt/bolt"
@@ -24,12 +25,23 @@ RDEPEND="
 "
 DEPEND="
 	${RDEPEND}
-	test? ( dev-util/umockdev )
+	test? (
+		dev-util/umockdev
+	)
 "
 BDEPEND="
 	app-text/asciidoc
 	dev-util/glib-utils
 	virtual/pkgconfig
+	test? (
+		dev-util/umockdev
+		${PYTHON_DEPS}
+		$(python_gen_any_dep \
+			'dev-python/pygobject[${PYTHON_USEDEP}]' \
+			'dev-python/dbus-python[${PYTHON_USEDEP}]' \
+			'dev-python/python-dbusmock[${PYTHON_USEDEP}]'
+		)
+	)
 "
 
 pkg_setup() {
@@ -44,6 +56,7 @@ pkg_setup() {
 	ERROR_HOTPLUG_PCI="Thunderbolt requires PCI hotplug support."
 
 	linux-info_pkg_setup
+	python-any-r1_pkg_setup
 }
 
 src_configure() {

commit a3827a78e7deecb2ff01fb0c8062661a4e1175d6

sys-apps/bolt: fix broken test support
```
 * Checking whether python3_14 is suitable ...
 *   dev-lang/python:3.14 ...                              [ ok ]
 * Using python3.14 to build (via PYTHON_COMPAT iteration)

[...]

meson.build:485: WARNING: Skipping integration test due to missing dependencies: No module named 'gi'

meson.build:485:2: ERROR: Fatal warnings enabled, aborting
```

By *default*, tests being skipped are merely a warning nobody notices,
resulting in 37 of 54 tests not being registered. This happened because:

- gen_any_dep takes a single arg, not many, that single arg is a
  dependency block

- if we use gen_any_deps, we also need to check if we have them, so even
  having them for PYTHON_COMPAT breaks if we *also* have python 3.14
  installed

- python is only added as a dependency inside `test? ( ...)` so we
  should never have unconditionally done pkg_setup?

The original bug report was likely caused by USE="-native-symlinks", so
the test lister script did not even execve() successfully, hence why
meson errored out at all. Usually it would successfully execve() so it
exiting with failure is handled by meson.build as automagic.

Fixes: 5ca8c25a6abf44d015bc807419059be4f5956b9a
Bug: https://bugs.gentoo.org/932003
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

sys-apps/bolt/bolt-0.9.10.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -36,13 +36,20 @@ BDEPEND="
 	test? (
 		dev-util/umockdev
 		${PYTHON_DEPS}
-		$(python_gen_any_dep \
-			'dev-python/pygobject[${PYTHON_USEDEP}]' \
-			'dev-python/dbus-python[${PYTHON_USEDEP}]' \
-			'dev-python/python-dbusmock[${PYTHON_USEDEP}]'
-		)
+		$(python_gen_any_dep '
+			dev-python/pygobject[${PYTHON_USEDEP}]
+			dev-python/dbus-python[${PYTHON_USEDEP}]
+			dev-python/python-dbusmock[${PYTHON_USEDEP}]
+		')
 	)
 "
+
+python_check_deps() {
+	python_has_version \
+		"dev-python/pygobject[${PYTHON_USEDEP}]" \
+		"dev-python/dbus-python[${PYTHON_USEDEP}]" \
+		"dev-python/python-dbusmock[${PYTHON_USEDEP}]"
+}
 
 pkg_setup() {
 	if use kernel_linux && kernel_is lt 5 6; then
@@ -56,7 +63,7 @@ pkg_setup() {
 	ERROR_HOTPLUG_PCI="Thunderbolt requires PCI hotplug support."
 
 	linux-info_pkg_setup
-	python-any-r1_pkg_setup
+	use test && python-any-r1_pkg_setup
 }
 
 src_configure() {

sys-apps/bolt/bolt-0.9.11.ebuild
@@ -36,13 +36,20 @@ BDEPEND="
 	test? (
 		dev-util/umockdev
 		${PYTHON_DEPS}
-		$(python_gen_any_dep \
-			'dev-python/pygobject[${PYTHON_USEDEP}]' \
-			'dev-python/dbus-python[${PYTHON_USEDEP}]' \
-			'dev-python/python-dbusmock[${PYTHON_USEDEP}]'
-		)
+		$(python_gen_any_dep '
+			dev-python/pygobject[${PYTHON_USEDEP}]
+			dev-python/dbus-python[${PYTHON_USEDEP}]
+			dev-python/python-dbusmock[${PYTHON_USEDEP}]
+		')
 	)
 "
+
+python_check_deps() {
+	python_has_version \
+		"dev-python/pygobject[${PYTHON_USEDEP}]" \
+		"dev-python/dbus-python[${PYTHON_USEDEP}]" \
+		"dev-python/python-dbusmock[${PYTHON_USEDEP}]"
+}
 
 pkg_setup() {
 	if use kernel_linux && kernel_is lt 5 6; then
@@ -56,7 +63,7 @@ pkg_setup() {
 	ERROR_HOTPLUG_PCI="Thunderbolt requires PCI hotplug support."
 
 	linux-info_pkg_setup
-	python-any-r1_pkg_setup
+	use test && python-any-r1_pkg_setup
 }
 
 src_configure() {

sys-apps/bolt/bolt-0.9.8-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -36,15 +36,22 @@ BDEPEND="
 	test? (
 		dev-util/umockdev
 		${PYTHON_DEPS}
-		$(python_gen_any_dep \
-			'dev-python/pygobject[${PYTHON_USEDEP}]' \
-			'dev-python/dbus-python[${PYTHON_USEDEP}]' \
-			'dev-python/python-dbusmock[${PYTHON_USEDEP}]'
-		)
+		$(python_gen_any_dep '
+			dev-python/pygobject[${PYTHON_USEDEP}]
+			dev-python/dbus-python[${PYTHON_USEDEP}]
+			dev-python/python-dbusmock[${PYTHON_USEDEP}]
+		')
 	)
 "
 
 PATCHES=( "${FILESDIR}"/${P}-sockaddr.patch )
+
+python_check_deps() {
+	python_has_version \
+		"dev-python/pygobject[${PYTHON_USEDEP}]" \
+		"dev-python/dbus-python[${PYTHON_USEDEP}]" \
+		"dev-python/python-dbusmock[${PYTHON_USEDEP}]"
+}
 
 pkg_setup() {
 	if use kernel_linux && kernel_is lt 5 6; then
@@ -58,7 +65,7 @@ pkg_setup() {
 	ERROR_HOTPLUG_PCI="Thunderbolt requires PCI hotplug support."
 
 	linux-info_pkg_setup
-	python-any-r1_pkg_setup
+	use test && python-any-r1_pkg_setup
 }
 
 src_configure() {

sys-apps/bolt/bolt-0.9.9.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -36,13 +36,20 @@ BDEPEND="
 	test? (
 		dev-util/umockdev
 		${PYTHON_DEPS}
-		$(python_gen_any_dep \
-			'dev-python/pygobject[${PYTHON_USEDEP}]' \
-			'dev-python/dbus-python[${PYTHON_USEDEP}]' \
-			'dev-python/python-dbusmock[${PYTHON_USEDEP}]'
-		)
+		$(python_gen_any_dep '
+			dev-python/pygobject[${PYTHON_USEDEP}]
+			dev-python/dbus-python[${PYTHON_USEDEP}]
+			dev-python/python-dbusmock[${PYTHON_USEDEP}]
+		')
 	)
 "
+
+python_check_deps() {
+	python_has_version \
+		"dev-python/pygobject[${PYTHON_USEDEP}]" \
+		"dev-python/dbus-python[${PYTHON_USEDEP}]" \
+		"dev-python/python-dbusmock[${PYTHON_USEDEP}]"
+}
 
 pkg_setup() {
 	if use kernel_linux && kernel_is lt 5 6; then
@@ -56,7 +63,7 @@ pkg_setup() {
 	ERROR_HOTPLUG_PCI="Thunderbolt requires PCI hotplug support."
 
 	linux-info_pkg_setup
-	python-any-r1_pkg_setup
+	use test && python-any-r1_pkg_setup
 }
 
 src_configure() {