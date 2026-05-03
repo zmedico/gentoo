zmedico/gentoo

commit 06381b2b42f524154ba44a00ad82be0b8b21d734

media-tv/kodi: re add system-ffmpeg
Postproc usage can be disabled.

Didn't fully remove the use flag eitherway...

See-Also: 3fcc535f57a8168d3715e80f7dd571df2ab4c1dd
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Signed-off-by: James Le Cuirot <chewi@gentoo.org>

media-tv/kodi/kodi-9999.ebuild
@@ -45,10 +45,12 @@ SRC_URI="
 	https://mirrors.kodi.tv/build-deps/sources/apache-groovy-binary-${GROOVY_VERSION}.zip
 	https://mirrors.kodi.tv/build-deps/sources/commons-lang3-${APACHE_COMMON_LANG_VERSION}-bin.tar.gz
 	https://mirrors.kodi.tv/build-deps/sources/commons-text-${APACHE_COMMON_TEXT_VERSION}-bin.tar.gz
-	https://ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.xz
 	css? (
 		https://github.com/xbmc/libdvdcss/archive/${LIBDVDCSS_VERSION}.tar.gz
 			-> libdvdcss-${LIBDVDCSS_VERSION}.tar.gz
+	)
+	!system-ffmpeg? (
+		https://ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.xz
 	)
 "
 if [[ ${PV} == *9999 ]] ; then
@@ -79,7 +81,7 @@ LICENSE+=" Apache-2.0"
 # libdvdnav, libdvdread and libdvdcss.
 LICENSE+=" GPL-2+"
 # ffmpeg built as USE="gpl"
-LICENSE+=" GPL-2"
+LICENSE+=" !system-ffmpeg? ( GPL-2 )"
 
 SLOT="0"
 # use flag is called libusb so that it doesn't fool people in thinking that
@@ -110,8 +112,6 @@ COMMON_DEPEND="
 	)
 "
 COMMON_TARGET_DEPEND="${PYTHON_DEPS}
-	app-arch/bzip2
-	app-arch/xz-utils
 	>=net-misc/curl-7.68.0[http2]
 	>=virtual/zlib-1.2.11:=
 	dev-db/sqlite:3
@@ -126,14 +126,12 @@ COMMON_TARGET_DEPEND="${PYTHON_DEPS}
 	dev-libs/tinyxml2:=
 	media-fonts/roboto
 	media-gfx/exiv2:=
-	media-libs/dav1d:=
 	media-libs/libglvnd[X?]
 	>=media-libs/freetype-2.10.1
 	media-libs/harfbuzz:=
 	>=media-libs/libass-0.15.0:=
 	media-libs/mesa[opengl,wayland?,X?]
 	media-libs/taglib:=
-	net-libs/gnutls:=
 	virtual/libiconv
 	virtual/ttf-fonts
 	x11-libs/libdrm
@@ -200,6 +198,15 @@ COMMON_TARGET_DEPEND="${PYTHON_DEPS}
 	)
 	samba? (
 		>=net-fs/samba-3.4.6[smbclient(+)]
+	)
+	system-ffmpeg? (
+		>=media-video/ffmpeg-7:=[encode(+),vaapi?,vdpau?,X?]
+	)
+	!system-ffmpeg? (
+		app-arch/bzip2
+		app-arch/xz-utils
+		media-libs/dav1d:=
+		net-libs/gnutls:=
 	)
 	udf? (
 		>=dev-libs/libudfread-1.0.0
@@ -394,8 +401,9 @@ src_configure() {
 		-DENABLE_VDPAU=$(usex vdpau)
 		-DENABLE_XSLT=$(usex xslt)
 
-		# ffmpeg hasn't decided on if postproc is removed or not and kodi has chosen to patch it back
+		#-DWITH_FFMPEG=$(usex system-ffmpeg)
 		-DWITH_FFMPEG=OFF
+		-DDISABLE_FFMPEG_SOURCE_PLUGINS=ON
 
 		#To bundle or not
 		-DENABLE_INTERNAL_ASS=OFF
@@ -403,7 +411,7 @@ src_configure() {
 		-DENABLE_INTERNAL_CROSSGUID=OFF
 		-DENABLE_INTERNAL_DAV1D=OFF
 		-DENABLE_INTERNAL_EXIV2=OFF
-		-DENABLE_INTERNAL_FFMPEG=ON
+		-DENABLE_INTERNAL_FFMPEG="$(usex !system-ffmpeg)"
 		-DENABLE_INTERNAL_FLATBUFFERS=OFF
 		-DENABLE_INTERNAL_FMT=OFF
 		-DENABLE_INTERNAL_FSTRCMP=OFF
@@ -419,12 +427,14 @@ src_configure() {
 		-Dgroovy_SOURCE_DIR="${WORKDIR}/groovy-${GROOVY_VERSION}"
 		-Dapache-commons-lang_SOURCE_DIR="${WORKDIR}/commons-lang3-${APACHE_COMMON_LANG_VERSION}"
 		-Dapache-commons-text_SOURCE_DIR="${WORKDIR}/commons-text-${APACHE_COMMON_TEXT_VERSION}"
-		-DFFMPEG_URL="${DISTDIR}/ffmpeg-${FFMPEG_VERSION}.tar.xz"
 	)
 
 	# Separated to avoid "Manually-specified variables were not used by the project:"
 	use cec && mycmakeargs+=( -DENABLE_INTERNAL_CEC=OFF )
 	use css && mycmakeargs+=( -Dlibdvdcss_URL="${DISTDIR}/libdvdcss-${LIBDVDCSS_VERSION}.tar.gz" )
+	use !system-ffmpeg && mycmakeargs+=(
+		-DFFMPEG_URL="${DISTDIR}/ffmpeg-${FFMPEG_VERSION}.tar.xz"
+	)
 	use nfs && mycmakeargs+=( -DENABLE_INTERNAL_NFS=OFF )
 	use !udev && mycmakeargs+=( -DENABLE_LIBUSB=$(usex libusb) )
 	use X && use !gles && mycmakeargs+=( -DENABLE_GLX=ON )

commit 89ce873da0a0f40ad3cf88353170c2b57d9f1b28

media-tv/kodi: move ENABLE_INTERNAL_DAV1D under !system-ffmpeg
Fixes: 06381b2b42f524154ba44a00ad82be0b8b21d734
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/815
Signed-off-by: James Le Cuirot <chewi@gentoo.org>

media-tv/kodi/kodi-9999.ebuild
@@ -424,7 +424,6 @@ src_configure() {
 		-DENABLE_INTERNAL_ASS=OFF
 		-DENABLE_INTERNAL_CROSSGUID=OFF
 		-DENABLE_INTERNAL_CURL=OFF
-		-DENABLE_INTERNAL_DAV1D=OFF
 		-DENABLE_INTERNAL_EXIV2=OFF
 		-DENABLE_INTERNAL_FFMPEG="$(usex !system-ffmpeg)"
 		-DENABLE_INTERNAL_FLATBUFFERS=OFF
@@ -454,6 +453,7 @@ src_configure() {
 		# Additional find_package on top of core_optional_deps for whatever reason
 		$(cmake_use_find_package vaapi VAAPI)
 		$(cmake_use_find_package vdpau VDPAU)
+		-DENABLE_INTERNAL_DAV1D=OFF
 		-DFFMPEG_URL="${DISTDIR}/ffmpeg-${FFMPEG_VERSION}.tar.xz"
 	)
 	use nfs && mycmakeargs+=( -DENABLE_INTERNAL_NFS=OFF )