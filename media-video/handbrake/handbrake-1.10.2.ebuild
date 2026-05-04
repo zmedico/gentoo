zmedico/gentoo

commit 94f365fb7050ac0b91b0f5dca8da6724a7758389

media-video/handbrake: hookup verify-sig
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://github.com/gentoo/gentoo/pull/45722
Signed-off-by: Sam James <sam@gentoo.org>

media-video/handbrake/handbrake-1.10.2.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -11,11 +11,19 @@ DESCRIPTION="Open-source, GPL-licensed, multiplatform, multithreaded video trans
 HOMEPAGE="https://handbrake.fr/ https://github.com/HandBrake/HandBrake"
 
 if [[ ${PV} == *9999* ]]; then
-	EGIT_REPO_URI="https://github.com/HandBrake/HandBrake.git"
 	inherit git-r3
+	EGIT_REPO_URI="https://github.com/HandBrake/HandBrake.git"
 else
+	inherit verify-sig
 	MY_P="HandBrake-${PV}"
-	SRC_URI="https://github.com/HandBrake/HandBrake/releases/download/${PV}/${MY_P}-source.tar.bz2 -> ${P}.tar.bz2"
+	SRC_URI="
+		https://github.com/HandBrake/HandBrake/releases/download/${PV}/${MY_P}-source.tar.bz2
+			-> ${P}.tar.bz2
+		verify-sig? (
+			https://github.com/HandBrake/HandBrake/releases/download/${PV}/${MY_P}-source.tar.bz2.sig
+				-> ${P}.tar.bz2.sig
+		)
+	"
 	S="${WORKDIR}/${MY_P}"
 	KEYWORDS="amd64 ~arm64 ~x86"
 fi
@@ -108,6 +116,10 @@ BDEPEND="
 		sys-devel/gettext
 	)
 "
+if [[ ${PV} != 9999 ]]; then
+	BDEPEND+="sec-keys/openpgp-keys-handbrake"
+	VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/handbrake.asc
+fi
 
 PATCHES=(
 	"${FILESDIR}"/handbrake-1.9.0-link-libdovi-properly.patch
@@ -120,6 +132,7 @@ src_unpack() {
 	if [[ ${PV} == 9999 ]]; then
 		git-r3_src_unpack
 	else
+		use verify-sig && verify-sig_verify_detached "${DISTDIR}"/${P}.tar.bz2{,.sig}
 		unpack ${P}.tar.bz2
 	fi
 }

media-video/handbrake/handbrake-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -11,11 +11,19 @@ DESCRIPTION="Open-source, GPL-licensed, multiplatform, multithreaded video trans
 HOMEPAGE="https://handbrake.fr/ https://github.com/HandBrake/HandBrake"
 
 if [[ ${PV} == *9999* ]]; then
-	EGIT_REPO_URI="https://github.com/HandBrake/HandBrake.git"
 	inherit git-r3
+	EGIT_REPO_URI="https://github.com/HandBrake/HandBrake.git"
 else
+	inherit verify-sig
 	MY_P="HandBrake-${PV}"
-	SRC_URI="https://github.com/HandBrake/HandBrake/releases/download/${PV}/${MY_P}-source.tar.bz2 -> ${P}.tar.bz2"
+	SRC_URI="
+		https://github.com/HandBrake/HandBrake/releases/download/${PV}/${MY_P}-source.tar.bz2
+			-> ${P}.tar.bz2
+		verify-sig? (
+			https://github.com/HandBrake/HandBrake/releases/download/${PV}/${MY_P}-source.tar.bz2.sig
+				-> ${P}.tar.bz2.sig
+		)
+	"
 	S="${WORKDIR}/${MY_P}"
 	KEYWORDS="~amd64 ~arm64 ~x86"
 fi
@@ -108,6 +116,10 @@ BDEPEND="
 		sys-devel/gettext
 	)
 "
+if [[ ${PV} != 9999 ]]; then
+	BDEPEND+="sec-keys/openpgp-keys-handbrake"
+	VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/handbrake.asc
+fi
 
 PATCHES=(
 	"${FILESDIR}"/handbrake-1.9.0-link-libdovi-properly.patch
@@ -120,6 +132,7 @@ src_unpack() {
 	if [[ ${PV} == 9999 ]]; then
 		git-r3_src_unpack
 	else
+		use verify-sig && verify-sig_verify_detached "${DISTDIR}"/${P}.tar.bz2{,.sig}
 		unpack ${P}.tar.bz2
 	fi
 }

media-video/handbrake/Manifest
@@ -1,4 +1,5 @@
 DIST handbrake-1.10.2.tar.bz2 9481895 BLAKE2B a7e2ec5cf5707d420eb01e561c2d57d5d93d21d1456483c84beb222a330e3ee96adefcd515f3512bac50d5417c3db35cfbd0f7cf2eba83e912f9470b8cd15768 SHA512 a1166ffb1597179e1ca58519cb6a7865ffbdedd5f1feda789cdb17cee94a9bb62b120926462bbc34b93c7ea53825099c45f70e809de7c456c88b60fce157b645
+DIST handbrake-1.10.2.tar.bz2.sig 566 BLAKE2B cde4ea4223ab9b346572637a27f7e6dfe07b97e56254355f8ab961e928c33630ac42c160d0d196b9e7ff897cf10089086a101b3ab4d93d59c79f4349cb6629d0 SHA512 9a33bc058a626c4bf1655abaaeae6641f7a71dd3db88ccb307e386623985791df7241e12ae6552f98a33b7a607b949cbe6fedaa9a36e5f16cd55fb44360455eb
 DIST handbrake-ffmpeg-7.1.1.tar.bz2 13278839 BLAKE2B dbedc190d1b752f07e0dc30452fe37a02300da75c3269956f09a63ada6f510838b3e6fb12f9882dbac88a82750ef42a16ed34b2214dfbdc56455b17a2cdc63f8 SHA512 db2682bd3f7dbfe4a33a5a7f0803010258d14f92e98ff2a5fdd17197422bc5cbfd992a63b93c9c5fb2b2420371a82e34a16e155f135a2aaf03764ed71adbc16b
 DIST handbrake-ffmpeg-8.0.tar.bz2 13685603 BLAKE2B c71bac40acae218bc57ff410c2079c36e7046ead9cb0f9b14c964d76d45509dcd382005f1082ac596d17c4e13470b53a152217a86768345efd136e07cfe920dd SHA512 4ec20d47d6752ea2f1ecb17b582a8bd1523dd9c12c5276dce2a56c75def27f97d4507abce216f350717092a5341299ad35334b300aa6822252d54c82d31b0853
 DIST handbrake-x265-snapshot-20250729-13276.tar.gz 1738652 BLAKE2B 59cba73b0d46cdaa40845e604e6b28ea428291d08b97ce144d13a072f164e18b0de20b2d4a62a92404ae32609a867d61e7b2d64099cac2124cc5c0640aad744a SHA512 50648176cf1e20e384be4145a525c193f515b73d3ff96ae2042b393ebb3ee3a8cab031e1c5314bdd67f53a6a00010226a5709cdb720dec47142a93c6828469af

commit 58d95bbc9d6fc680e628948c59c1b6b62bdb3c5c

media-video/handbrake: signature dependency should be conditional
Fixes: 94f365fb7050ac0b91b0f5dca8da6724a7758389
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/823
Merges: https://codeberg.org/gentoo/gentoo/pulls/823
Signed-off-by: Sam James <sam@gentoo.org>

media-video/handbrake/handbrake-1.10.2.ebuild
@@ -118,7 +118,7 @@ BDEPEND="
 	)
 "
 if [[ ${PV} != 9999 ]]; then
-	BDEPEND+="sec-keys/openpgp-keys-handbrake"
+	BDEPEND+=" verify-sig? ( sec-keys/openpgp-keys-handbrake )"
 	VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/handbrake.asc
 fi
 

media-video/handbrake/handbrake-1.11.1.ebuild
@@ -117,7 +117,7 @@ BDEPEND="
 	)
 "
 if [[ ${PV} != 9999 ]]; then
-	BDEPEND+=">=sec-keys/openpgp-keys-handbrake-20260311"
+	BDEPEND+=" verify-sig? ( >=sec-keys/openpgp-keys-handbrake-20260311 )"
 	VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/handbrake.asc
 fi
 

media-video/handbrake/handbrake-9999.ebuild
@@ -117,7 +117,7 @@ BDEPEND="
 	)
 "
 if [[ ${PV} != 9999 ]]; then
-	BDEPEND+=">=sec-keys/openpgp-keys-handbrake-20260311"
+	BDEPEND+=" verify-sig? ( >=sec-keys/openpgp-keys-handbrake-20260311 )"
 	VERIFY_SIG_OPENPGP_KEY_PATH=/usr/share/openpgp-keys/handbrake.asc
 fi
 