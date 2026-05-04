zmedico/gentoo

commit e60c97194444e959d2bd20f96b26300e11f48a96

media-video/ffmpeg: Add soc patch for 8.1 and udev rule for dma_heap
This is different from previous soc patches in that it benefits all boards with
VPU drivers, not just the Raspberry Pi. I had previously thought the RPi patch
would help other boards, but the H.264 decoder is stateful when almost all other
boards do stateless decoding. See the patch header for more details.

The udev rule is needed for stateful decoding with Kodi.

Signed-off-by: James Le Cuirot <chewi@gentoo.org>

media-video/ffmpeg/ffmpeg-8.1.ebuild
@@ -3,9 +3,9 @@
 
 EAPI=8
 
-inherit flag-o-matic multilib-minimal toolchain-funcs
+inherit flag-o-matic multilib-minimal toolchain-funcs udev
 
-FFMPEG_SOC_PATCH=
+FFMPEG_SOC_PATCH=ffmpeg-soc-8.1.patch
 FFMPEG_SUBSLOT=60.62.62 # avutil.avcodec.avformat SONAME
 
 if [[ ${PV} == 9999 ]]; then
@@ -500,7 +500,6 @@ multilib_src_configure() {
 
 	in_iuse soc && use soc &&
 		conf+=(
-			--disable-epoxy
 			--enable-libudev
 			--enable-sand
 			--enable-v4l2-request
@@ -604,4 +603,17 @@ multilib_src_install() {
 	emake V=1 DESTDIR="${D}" install
 	in_iuse chromium && use chromium && multilib_is_native_abi &&
 		emake V=1 DESTDIR="${D}" install-libffmpeg
+}
+
+multilib_src_install_all() {
+	in_iuse soc && use soc && udev_dorules "${FILESDIR}"/60-dma-heap-ffmpeg.rules
+	einstalldocs
+}
+
+pkg_postinst() {
+	in_iuse soc && use soc udev_reload
+}
+
+pkg_postrm() {
+	in_iuse soc && use soc udev_reload
 }

media-video/ffmpeg/ffmpeg-9999.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-inherit flag-o-matic multilib-minimal toolchain-funcs
+inherit flag-o-matic multilib-minimal toolchain-funcs udev
 
 FFMPEG_SOC_PATCH=
 FFMPEG_SUBSLOT=60.62.62 # avutil.avcodec.avformat SONAME
@@ -503,7 +503,6 @@ multilib_src_configure() {
 
 	in_iuse soc && use soc &&
 		conf+=(
-			--disable-epoxy
 			--enable-libudev
 			--enable-sand
 			--enable-v4l2-request
@@ -607,4 +606,17 @@ multilib_src_install() {
 	emake V=1 DESTDIR="${D}" install
 	in_iuse chromium && use chromium && multilib_is_native_abi &&
 		emake V=1 DESTDIR="${D}" install-libffmpeg
+}
+
+multilib_src_install_all() {
+	in_iuse soc && use soc && udev_dorules "${FILESDIR}"/60-dma-heap-ffmpeg.rules
+	einstalldocs
+}
+
+pkg_postinst() {
+	in_iuse soc && use soc udev_reload
+}
+
+pkg_postrm() {
+	in_iuse soc && use soc udev_reload
 }

media-video/ffmpeg/Manifest
@@ -17,3 +17,4 @@ DIST ffmpeg-8.1.tar.xz.asc 520 BLAKE2B 5bbf0a315c5d8db98b6e1ffd98bc51577b0dd3150
 DIST ffmpeg-rpi-6.1-r3.patch 800738 BLAKE2B b7e449ee5eec8b50a3b3bba233c5269f3b6845dbd18a619b844c841a97345ce24c7a5d7f4da05b08e3cb49668426b9044f3d3553d9c2ef3a50aec7199bfa3c09 SHA512 aeaf12afabf5f2192fb0b6ef47397d8477b109e49cee8c02a890d1656a27b1985c62c719ef72966d8c97b0709918233b65ea2e0ef988771507e4be137fcd9158
 DIST ffmpeg-rpi-7.1.1.patch 847869 BLAKE2B efc3fef1810034b40eee1f637499171529fc6027c4fc0ef66da587c704a7742b454903aa060b21305e1d9a714eb31d96cd93f93d7cadb5815a0de22ac040b9e1 SHA512 a3f9013aec9b17e0fc1689d946e57e251a88eac3f9e7856d46fe9614fea29211feaf106d32a31f97c0e8c83c1685ca454c31f9bcb5dafb669a0933d683e581c8
 DIST ffmpeg-rpi-8.0.patch 850353 BLAKE2B 17c97bc7f1eb1d514a946780cf9c96520d6f09061c5e3d4add323d36fa0ebefef337ed9d1e1398a07547e37218ba20fc9a9677d3fc8e0c65d3ae2b2a63556e8a SHA512 b4692d704a5256001147260e9345bf148e4f0fb492acefaf38793821a9bad53409702cb08eff039c82eeba8bc17cd5f994a463680e18ff3fcfae5905ae3335e9
+DIST ffmpeg-soc-8.1.patch 702636 BLAKE2B 425247f9f41d13d706a9f7de00c14ad5606987e10a01c320c0b6a9091e0f340d0c34230b1a945c53c8bddc472105a6a6dbd96421e11f855f9427d1c4f7342028 SHA512 f24ae85846d98b1bdcde0d80916236c83497042edd6eb6c7b7f650c4fe787a04b6aa56cee89afa1f115501dd65edf2b120e4997b02d5f8e8a4a46edfd5ead387

media-video/ffmpeg/files/60-dma-heap-ffmpeg.rules
@@ -1,0 +1,1 @@
+SUBSYSTEM=="dma_heap", KERNEL=="linux,cma", GROUP="video", MODE="0660"

commit ab1000a5fbcf47963554dadcafcb7f0d6ac85929

media-video/ffmpeg: fix typo
Thankfully `use` having a 2nd argument does not seem to cause `die`
(or at least not with portage) which may have been annoying for
users, and udev_reload not being ran isn't the end of the world.

Fixes: e60c97194444e959d2bd20f96b26300e11f48a96
Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

media-video/ffmpeg/ffmpeg-8.1.ebuild
@@ -611,9 +611,9 @@ multilib_src_install_all() {
 }
 
 pkg_postinst() {
-	in_iuse soc && use soc udev_reload
+	in_iuse soc && use soc && udev_reload
 }
 
 pkg_postrm() {
-	in_iuse soc && use soc udev_reload
+	in_iuse soc && use soc && udev_reload
 }

media-video/ffmpeg/ffmpeg-9999.ebuild
@@ -614,9 +614,9 @@ multilib_src_install_all() {
 }
 
 pkg_postinst() {
-	in_iuse soc && use soc udev_reload
+	in_iuse soc && use soc && udev_reload
 }
 
 pkg_postrm() {
-	in_iuse soc && use soc udev_reload
+	in_iuse soc && use soc && udev_reload
 }