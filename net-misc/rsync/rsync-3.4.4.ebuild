zmedico/gentoo

commit 6562782921399caf4ec64ec916f2e6acda8f16cd

net-misc/rsync: tests need Python now
Signed-off-by: Sam James <sam@gentoo.org>

net-misc/rsync/rsync-3.4.4.ebuild
@@ -37,9 +37,12 @@ fi
 
 LICENSE="GPL-3"
 SLOT="0"
-IUSE="acl examples iconv lz4 rrsync ssl stunnel system-zlib xattr +xxhash zstd"
-REQUIRED_USE+=" examples? ( ${PYTHON_REQUIRED_USE} )"
-REQUIRED_USE+=" rrsync? ( ${PYTHON_REQUIRED_USE} )"
+IUSE="acl examples iconv lz4 rrsync ssl stunnel system-zlib test xattr +xxhash zstd"
+RESTRICT="!test? ( test )"
+REQUIRED_USE+="
+	examples? ( ${PYTHON_REQUIRED_USE} )
+	rrsync? ( ${PYTHON_REQUIRED_USE} )
+"
 
 RDEPEND="
 	>=dev-libs/popt-1.19
@@ -64,13 +67,16 @@ DEPEND="${RDEPEND}"
 BDEPEND="
 	examples? ( ${PYTHON_DEPS} )
 	rrsync? ( ${PYTHON_DEPS} )
+	test? ( ${PYTHON_DEPS} )
 "
 
 if [[ ${PV} == *9999 ]] ; then
-	BDEPEND+=" ${PYTHON_DEPS}
+	BDEPEND+="
+		${PYTHON_DEPS}
 		$(python_gen_cond_dep '
 			dev-python/commonmark[${PYTHON_USEDEP}]
-		')"
+		')
+	"
 else
 	BDEPEND+=" verify-sig? ( sec-keys/openpgp-keys-andrewtridgell )"
 fi
@@ -78,7 +84,7 @@ fi
 pkg_setup() {
 	# - USE=examples needs Python itself at runtime, but nothing else
 	# - 9999 needs commonmark at build time
-	if [[ ${PV} == *9999 ]] || use examples || use rrsync; then
+	if [[ ${PV} == *9999 ]] || use examples || use rrsync || use test ; then
 		python-single-r1_pkg_setup
 	fi
 }

net-misc/rsync/rsync-9999.ebuild
@@ -37,9 +37,12 @@ fi
 
 LICENSE="GPL-3"
 SLOT="0"
-IUSE="acl examples iconv lz4 rrsync ssl stunnel system-zlib xattr +xxhash zstd"
-REQUIRED_USE+=" examples? ( ${PYTHON_REQUIRED_USE} )"
-REQUIRED_USE+=" rrsync? ( ${PYTHON_REQUIRED_USE} )"
+IUSE="acl examples iconv lz4 rrsync ssl stunnel system-zlib test xattr +xxhash zstd"
+RESTRICT="!test? ( test )"
+REQUIRED_USE+="
+	examples? ( ${PYTHON_REQUIRED_USE} )
+	rrsync? ( ${PYTHON_REQUIRED_USE} )
+"
 
 RDEPEND="
 	>=dev-libs/popt-1.19
@@ -64,13 +67,16 @@ DEPEND="${RDEPEND}"
 BDEPEND="
 	examples? ( ${PYTHON_DEPS} )
 	rrsync? ( ${PYTHON_DEPS} )
+	test? ( ${PYTHON_DEPS} )
 "
 
 if [[ ${PV} == *9999 ]] ; then
-	BDEPEND+=" ${PYTHON_DEPS}
+	BDEPEND+="
+		${PYTHON_DEPS}
 		$(python_gen_cond_dep '
 			dev-python/commonmark[${PYTHON_USEDEP}]
-		')"
+		')
+	"
 else
 	BDEPEND+=" verify-sig? ( sec-keys/openpgp-keys-andrewtridgell )"
 fi
@@ -78,7 +84,7 @@ fi
 pkg_setup() {
 	# - USE=examples needs Python itself at runtime, but nothing else
 	# - 9999 needs commonmark at build time
-	if [[ ${PV} == *9999 ]] || use examples || use rrsync; then
+	if [[ ${PV} == *9999 ]] || use examples || use rrsync || use test ; then
 		python-single-r1_pkg_setup
 	fi
 }

commit bf19762452a2098b3ea9d7a438014aedf45608be

net-misc/rsync: missing test? ( ${PYTHON_REQUIRED_USE} )
Fixes: 6562782921399caf4ec64ec916f2e6acda8f16cd
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/1169
Merges: https://codeberg.org/gentoo/gentoo/pulls/1169
Signed-off-by: Sam James <sam@gentoo.org>

net-misc/rsync/rsync-3.4.4.ebuild
@@ -42,6 +42,7 @@ RESTRICT="!test? ( test )"
 REQUIRED_USE+="
 	examples? ( ${PYTHON_REQUIRED_USE} )
 	rrsync? ( ${PYTHON_REQUIRED_USE} )
+	test? ( ${PYTHON_REQUIRED_USE} )
 "
 
 RDEPEND="

net-misc/rsync/rsync-9999.ebuild
@@ -42,6 +42,7 @@ RESTRICT="!test? ( test )"
 REQUIRED_USE+="
 	examples? ( ${PYTHON_REQUIRED_USE} )
 	rrsync? ( ${PYTHON_REQUIRED_USE} )
+	test? ( ${PYTHON_REQUIRED_USE} )
 "
 
 RDEPEND="