zmedico/gentoo

commit c3eb96c02a89d301417370d54b290693cc003e73

mail-filter/rspamd: depend on older doctest for all versoins
On the binhost, we hit with 3.14.3:
```
/usr/include/doctest/doctest.h:1277:32: error: passing ‘const rspamd::mime::basic_mime_string<char>’ as ‘this’ argument discards qualifiers [-fpermissive]
 1277 |         for (auto it = in.begin(); it != in.end(); ++it) {
      |                        ~~~~~~~~^~
```

Bug: https://bugs.gentoo.org/973396
Signed-off-by: Sam James <sam@gentoo.org>

mail-filter/rspamd/rspamd-4.1.1.ebuild
@@ -57,7 +57,7 @@ RDEPEND="
 "
 DEPEND="
 	${RDEPEND}
-	dev-cpp/doctest
+	<dev-cpp/doctest-2.5
 	>=dev-libs/xxhash-0.8.0
 "
 BDEPEND="

mail-filter/rspamd/rspamd-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -58,7 +58,7 @@ RDEPEND="
 "
 DEPEND="
 	${RDEPEND}
-	dev-cpp/doctest
+	<dev-cpp/doctest-2.5
 	>=dev-libs/xxhash-0.8.0
 "
 BDEPEND="

mail-filter/rspamd/rspamd-3.13.2.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2025 Gentoo Authors
+# Copyright 1999-2026 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -58,7 +58,7 @@ RDEPEND="
 "
 DEPEND="
 	${RDEPEND}
-	dev-cpp/doctest
+	<dev-cpp/doctest-2.5
 	>=dev-libs/xxhash-0.8.0
 "
 BDEPEND="

mail-filter/rspamd/rspamd-3.14.3.ebuild
@@ -58,7 +58,7 @@ RDEPEND="
 "
 DEPEND="
 	${RDEPEND}
-	dev-cpp/doctest
+	<dev-cpp/doctest-2.5
 	>=dev-libs/xxhash-0.8.0
 "
 BDEPEND="

commit c6fb64ea5e9751ace7c35b9b39d1eb55089b2ba4

mail-filter/rspamd: newer is fixed w/ doctest
Bug: https://bugs.gentoo.org/973396
Fixes: c3eb96c02a89d301417370d54b290693cc003e73
Signed-off-by: Sam James <sam@gentoo.org>

mail-filter/rspamd/rspamd-4.1.1.ebuild
@@ -57,7 +57,7 @@ RDEPEND="
 "
 DEPEND="
 	${RDEPEND}
-	<dev-cpp/doctest-2.5
+	dev-cpp/doctest
 	>=dev-libs/xxhash-0.8.0
 "
 BDEPEND="

mail-filter/rspamd/rspamd-9999.ebuild
@@ -58,7 +58,7 @@ RDEPEND="
 "
 DEPEND="
 	${RDEPEND}
-	<dev-cpp/doctest-2.5
+	dev-cpp/doctest
 	>=dev-libs/xxhash-0.8.0
 "
 BDEPEND="