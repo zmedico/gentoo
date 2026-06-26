zmedico/gentoo

commit 295d886cf3a446ea6232ca487a361daf603e093b

media-libs/mlt: add 7.38.0
* Re-enable tests by piggy backing on kwalify being optional for MSVC.
* Add sox use flag.
* Reorder options to match upstream.
* Enable py3.15.
* Check dependencies.

Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/899
Signed-off-by: Sam James <sam@gentoo.org>

media-libs/mlt/mlt-7.38.0.ebuild
@@ -1,0 +1,189 @@
+# Copyright 1999-2026 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+PYTHON_COMPAT=( python3_{11..15} )
+inherit python-single-r1 cmake flag-o-matic
+
+DESCRIPTION="Open source multimedia framework for television broadcasting"
+HOMEPAGE="https://www.mltframework.org/"
+SRC_URI="https://github.com/mltframework/${PN}/releases/download/v${PV}/${P}.tar.gz"
+
+LICENSE="GPL-3"
+SLOT="0/7"
+KEYWORDS="~amd64 ~arm64 ~loong ~ppc64 ~riscv ~x86"
+
+IUSE="debug ffmpeg frei0r gtk jack libsamplerate opencv opengl python qt6
+rtaudio rubberband sdl sox test vdpau vidstab vorbis xine xml"
+REQUIRED_USE="python? ( ${PYTHON_REQUIRED_USE} )"
+
+RESTRICT="!test? ( test )"
+
+# rtaudio will use OSS on non linux OSes
+# Qt already needs FFTW/PLUS so let's just always have it on to ensure
+# MLT is useful: bug #603168.
+DEPEND="
+	>=media-libs/libebur128-1.2.2:=
+	sci-libs/fftw:3.0=
+	ffmpeg? ( media-video/ffmpeg:0=[vdpau?] )
+	frei0r? ( media-plugins/frei0r-plugins )
+	gtk? (
+		media-libs/fontconfig
+		media-libs/libexif
+		x11-libs/gdk-pixbuf:2
+		x11-libs/pango
+	)
+	jack? (
+		dev-libs/glib:2
+		>=dev-libs/libxml2-2.5:=
+		media-libs/ladspa-sdk
+		virtual/jack
+	)
+	libsamplerate? ( >=media-libs/libsamplerate-0.1.2 )
+	opencv? (
+		>=media-libs/opencv-4.5.1:=[contrib]
+		|| (
+			media-libs/opencv[ffmpeg]
+			media-libs/opencv[gstreamer]
+		)
+	)
+	opengl? (
+		media-libs/libglvnd
+		media-video/movit
+	)
+	python? ( ${PYTHON_DEPS} )
+	qt6? (
+		dev-qt/qtbase:6[gui,network,opengl,widgets,xml]
+		dev-qt/qtsvg:6
+		media-libs/libexif
+		x11-libs/libX11
+	)
+	rtaudio? (
+		>=media-libs/rtaudio-4.1.2:=
+		kernel_linux? ( media-libs/alsa-lib )
+	)
+	rubberband? ( media-libs/rubberband:= )
+	sdl? (
+		media-libs/libsdl2[X,opengl,video]
+		media-libs/sdl2-image
+	)
+	sox? ( media-sound/sox:= )
+	vidstab? ( media-libs/vidstab )
+	vorbis? ( media-libs/libvorbis )
+	xine? ( >=media-libs/xine-lib-1.1.2_pre20060328-r7 )
+	xml? ( >=dev-libs/libxml2-2.5:= )
+"
+#	java? ( >=virtual/jre-1.8:* )
+#	perl? ( dev-lang/perl )
+#	php? ( dev-lang/php )
+#	ruby? ( ${RUBY_DEPS} )
+#	tcl? ( dev-lang/tcl:0= )
+RDEPEND="${DEPEND}"
+BDEPEND="
+	virtual/pkgconfig
+	python? ( >=dev-lang/swig-2.0 )
+"
+
+DOCS=( AUTHORS NEWS README.md )
+
+PATCHES=(
+	# downstream
+	"${FILESDIR}"/${PN}-6.10.0-swig-underlinking.patch
+	"${FILESDIR}"/${PN}-6.22.1-no_lua_bdepend.patch
+	"${FILESDIR}"/${PN}-7.0.1-cmake-symlink.patch
+)
+
+pkg_setup() {
+	use python && python-single-r1_pkg_setup
+}
+
+src_prepare() {
+	# Respect CFLAGS LDFLAGS when building shared libraries. Bug #308873
+	if use python; then
+		sed -i "/mlt.so/s/ -lmlt++ /& ${CFLAGS} ${LDFLAGS} /" src/swig/python/build || die
+		python_fix_shebang src/swig/python
+	fi
+
+	# Workaround kwalify dependency. Its not required on MSVC, we can ignore it as well.
+	sed -e '/find_package(Kwalify/ s/REQUIRED//' -i CMakeLists.txt || die
+
+	cmake_src_prepare
+}
+
+src_configure() {
+	# Workaround for bug #919981
+	append-ldflags $(test-flags-CCLD -Wl,--undefined-version)
+
+	# match order in CMakeLists.txt
+	local mycmakeargs=(
+		-DCMAKE_SKIP_RPATH=ON
+
+		-DGPL=ON
+		-DGPL3=ON
+		-DBUILD_TESTING=$(usex test)
+		-DCLANG_FORMAT=OFF
+		-DBUILD_TESTS_WITH_QT6=ON
+
+		-DMOD_AVFORMAT=$(usex ffmpeg)
+		#-DMOD_DECKLINK=
+		-DMOD_FREI0R=$(usex frei0r)
+		-DMOD_GDK=$(usex gtk)
+		-DMOD_GLAXNIMATE_QT6=$(usex qt6)
+		-DMOD_JACKRACK=$(usex jack)
+		-DUSE_LV2=OFF	# TODO
+		-DUSE_VST2=OFF	# TODO
+		-DMOD_KDENLIVE=ON
+		-DMOD_MOVIT=$(usex opengl)
+		#-DMOD_NDI=
+		#-DMOD_NORMALIZE=
+		#-DMOD_OLDFILM=
+		-DMOD_OPENCV=$(usex opencv)
+		# -DMOD_OPENFX=
+		-DMOD_PLUS=ON
+		#-DMOD_PLUSGPL=
+		-DMOD_QT6=$(usex qt6)
+		-DMOD_RESAMPLE=$(usex libsamplerate)
+		-DMOD_RTAUDIO=$(usex rtaudio)
+		-DMOD_RUBBERBAND=$(usex rubberband)
+		-DMOD_SDL1=OFF
+		-DMOD_SDL2=$(usex sdl)
+		-DMOD_SOX=$(usex sox)
+		-DMOD_SPATIALAUDIO=OFF # TODO: package libspatialaudio
+		-DMOD_VIDSTAB=$(usex vidstab)
+		-DMOD_VORBIS=$(usex vorbis)
+		-DMOD_XINE=$(usex xine)
+		-DMOD_XML=$(usex xml)
+	)
+
+	# TODO: rework upstream CMake to allow controlling MMX/SSE/SSE2
+	# TODO: add swig language bindings?
+	# see also https://www.mltframework.org/twiki/bin/view/MLT/ExtremeMakeover
+
+	if use python; then
+		mycmakeargs+=(
+			-DSWIG_PYTHON=ON
+			-DPython3_EXECUTABLE="${PYTHON}"
+		)
+	fi
+
+	cmake_src_configure
+}
+
+src_install() {
+	cmake_src_install
+
+	insinto /usr/share/${PN}
+	doins -r demo
+
+	#
+	# Install SWIG bindings
+	#
+
+	docinto swig
+
+	if use python; then
+		dodoc "${S}"/src/swig/python/play.py
+		python_optimize
+	fi
+}

media-libs/mlt/Manifest
@@ -1,2 +1,3 @@
 DIST mlt-7.32.0.tar.gz 1790860 BLAKE2B e88f1517ce9cb4e372c9618124dc2623d30db4262c86a60831c4f1033561cd10b6bd024f12789cf9aef583e403dcf5f3c309d5283df23a1537569d7ad4118318 SHA512 ac909376d3d7432244aa4c6ac7d0a4bcaeaae9668e27d6f9633f190858374df7dc7c8477bc12be2ec02036638f790fc4ef256123516f5d901d49b6432d95aa3e
 DIST mlt-7.34.1.tar.gz 1817867 BLAKE2B 45a422b12dca99fcd95af9f2c540497e2aa97210f6674726884b0eb8d1f321706679512675ff21993c997d9332be7128ce93f689c8f7612d68952ea87e6c56e5 SHA512 de71d698e8adc03d4e3c66c5e1d4dbf2e91848b2260e29084401a56d49493f28671c5c0979922dc46864ee3991ef35cab2cd9be6b88bb67dc49e36488f87050e
+DIST mlt-7.38.0.tar.gz 6674009 BLAKE2B 4843dbca30cdec26b3b340901ca1a3603bb9ce6565620218d017f0b848e1991fcc3cf0fe6354ec95d9c557cbcfd6e9f0e5a61f3aa9653b1c92822a4fb941af8c SHA512 762f7470eb294e6923206d9793ec65341eb6712c5b0251366dcd565ef84f59a0dc9c01a0695ce2c6b2f427d3a99fc38207ccf92905f105325720af9ce7e534b8

commit 90b072dd398be66f82e170a3fdd287e46b59ab3d

media-libs/mlt: tests require qt
Closes: https://bugs.gentoo.org/978043
Fixes: 295d886cf3a446ea6232ca487a361daf603e093b
Signed-off-by: Alfred Wingate <parona@protonmail.com>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/1260
Signed-off-by: Andreas Sturmlechner <asturm@gentoo.org>

media-libs/mlt/mlt-7.38.0.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-PYTHON_COMPAT=( python3_{11..15} )
+PYTHON_COMPAT=( python3_{12..15} )
 inherit python-single-r1 cmake flag-o-matic
 
 DESCRIPTION="Open source multimedia framework for television broadcasting"
@@ -23,7 +23,7 @@ RESTRICT="!test? ( test )"
 # rtaudio will use OSS on non linux OSes
 # Qt already needs FFTW/PLUS so let's just always have it on to ensure
 # MLT is useful: bug #603168.
-DEPEND="
+RDEPEND="
 	>=media-libs/libebur128-1.2.2:=
 	sci-libs/fftw:3.0=
 	ffmpeg? ( media-video/ffmpeg:0=[vdpau?] )
@@ -79,7 +79,9 @@ DEPEND="
 #	php? ( dev-lang/php )
 #	ruby? ( ${RUBY_DEPS} )
 #	tcl? ( dev-lang/tcl:0= )
-RDEPEND="${DEPEND}"
+DEPEND="${RDEPEND}
+	test? ( dev-qt/qtbase:6 )
+"
 BDEPEND="
 	virtual/pkgconfig
 	python? ( >=dev-lang/swig-2.0 )
@@ -123,7 +125,7 @@ src_configure() {
 		-DGPL3=ON
 		-DBUILD_TESTING=$(usex test)
 		-DCLANG_FORMAT=OFF
-		-DBUILD_TESTS_WITH_QT6=ON
+		-DBUILD_TESTS_WITH_QT6=ON # The tests use qttest, this switch decides whether qt5 or qt6 is used.
 
 		-DMOD_AVFORMAT=$(usex ffmpeg)
 		#-DMOD_DECKLINK=