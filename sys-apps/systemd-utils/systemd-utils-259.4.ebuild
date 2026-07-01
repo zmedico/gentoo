zmedico/gentoo

commit f4d1ca638244e343b859fbd3fedba71119660704

sys-apps/systemd-utils: disable -D_FORTIFY_SOURCE=3 for Clang
It breaks booting. It's likely that the malloc_usable_size trick is the
cause again. Someone interested in Clang issues could debug why the previous
upstream workaround doesn't seem to work anymore.

Bug: https://bugs.gentoo.org/971773
Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/systemd-utils/systemd-utils-259.4.ebuild
@@ -9,8 +9,8 @@ QA_PKGCONFIG_VERSION=$(ver_cut 1)
 TMPFILES_OPTIONAL=1
 UDEV_OPTIONAL=1
 
-inherit linux-info meson-multilib
-inherit python-single-r1 secureboot shell-completion udev
+inherit flag-o-matic linux-info meson-multilib
+inherit python-single-r1 secureboot shell-completion toolchain-funcs udev
 
 DESCRIPTION="Utilities split out from systemd for OpenRC users"
 HOMEPAGE="https://systemd.io/"
@@ -120,6 +120,21 @@ pkg_setup() {
 
 src_configure() {
 	python_setup
+
+	# Our toolchain sets F_S=2 by default w/ >= -O2, so we need
+	# to unset F_S first, then explicitly set 2, to negate any default
+	# and anything set by the user if they're choosing 3 (or if they've
+	# modified GCC to set 3).
+	#
+	# malloc_usable_size doesn't play well with _F_S=3:
+	#  https://github.com/systemd/systemd/issues/41459 (bug #971773)
+	if tc-is-clang && tc-enables-fortify-source ; then
+		# We can't unconditionally do this b/c we fortify needs
+		# some level of optimisation.
+		filter-flags -D_FORTIFY_SOURCE=3
+		append-cppflags -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2
+	fi
+
 	meson-multilib_src_configure
 }
 

sys-apps/systemd-utils/systemd-utils-260.1-r1.ebuild
@@ -9,8 +9,8 @@ QA_PKGCONFIG_VERSION=$(ver_cut 1)
 TMPFILES_OPTIONAL=1
 UDEV_OPTIONAL=1
 
-inherit linux-info meson-multilib
-inherit python-single-r1 secureboot shell-completion udev
+inherit flag-o-matic linux-info meson-multilib
+inherit python-single-r1 secureboot shell-completion toolchain-funcs udev
 
 DESCRIPTION="Utilities split out from systemd for OpenRC users"
 HOMEPAGE="https://systemd.io/"
@@ -118,6 +118,21 @@ pkg_setup() {
 
 src_configure() {
 	python_setup
+
+	# Our toolchain sets F_S=2 by default w/ >= -O2, so we need
+	# to unset F_S first, then explicitly set 2, to negate any default
+	# and anything set by the user if they're choosing 3 (or if they've
+	# modified GCC to set 3).
+	#
+	# malloc_usable_size doesn't play well with _F_S=3:
+	#  https://github.com/systemd/systemd/issues/41459 (bug #971773)
+	if tc-is-clang && tc-enables-fortify-source ; then
+		# We can't unconditionally do this b/c we fortify needs
+		# some level of optimisation.
+		filter-flags -D_FORTIFY_SOURCE=3
+		append-cppflags -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2
+	fi
+
 	meson-multilib_src_configure
 }
 

commit f982c0238cb037c2a2d4856bd41b047c0d51f5ab

sys-apps/systemd-utils: fix Clang workaround
For Clang, we need another method to disable _F_S being automatically
added.

Bug: https://bugs.gentoo.org/971773
Fixes: f4d1ca638244e343b859fbd3fedba71119660704
Signed-off-by: Sam James <sam@gentoo.org>

sys-apps/systemd-utils/systemd-utils-259.4.ebuild
@@ -132,7 +132,7 @@ src_configure() {
 		# We can't unconditionally do this b/c we fortify needs
 		# some level of optimisation.
 		filter-flags -D_FORTIFY_SOURCE=3
-		append-cppflags -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2
+		append-cppflags -U_FORTIFY_SOURCE -D_GENTOO_NO_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2
 	fi
 
 	meson-multilib_src_configure

sys-apps/systemd-utils/systemd-utils-260.1-r1.ebuild
@@ -130,7 +130,7 @@ src_configure() {
 		# We can't unconditionally do this b/c we fortify needs
 		# some level of optimisation.
 		filter-flags -D_FORTIFY_SOURCE=3
-		append-cppflags -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2
+		append-cppflags -U_FORTIFY_SOURCE -D_GENTOO_NO_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2
 	fi
 
 	meson-multilib_src_configure