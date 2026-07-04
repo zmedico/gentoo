zmedico/gentoo

commit 36aab3ec3df519784c40ef762b47caceb2a3b6bb

dev-cpp/glibmm: add 2.84.0
Closes: https://bugs.gentoo.org/942313
Signed-off-by: Pacho Ramos <pacho@gentoo.org>

dev-cpp/glibmm/glibmm-2.84.0.ebuild
@@ -1,0 +1,52 @@
+# Copyright 1999-2025 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+PYTHON_COMPAT=( python3_{11..13} )
+inherit gnome.org meson-multilib python-any-r1
+
+DESCRIPTION="C++ interface for glib2"
+HOMEPAGE="https://www.gtkmm.org https://gitlab.gnome.org/GNOME/glibmm"
+
+LICENSE="LGPL-2.1+"
+SLOT="2.68"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~loong ~mips ~ppc ~ppc64 ~riscv ~sparc ~x86 ~amd64-linux ~x86-linux ~ppc-macos"
+IUSE="gtk-doc debug test"
+RESTRICT="!test? ( test )"
+
+RDEPEND="
+	>=dev-libs/glib-2.83.4:2[${MULTILIB_USEDEP}]
+	dev-libs/libsigc++:3[gtk-doc?,${MULTILIB_USEDEP}]
+"
+DEPEND="${RDEPEND}"
+BDEPEND="
+	${PYTHON_DEPS}
+	virtual/pkgconfig
+	gtk-doc? (
+		app-text/doxygen[dot]
+		dev-lang/perl
+		dev-libs/libxslt
+	)
+"
+
+src_prepare() {
+	default
+
+	# giomm_tls_client requires FEATURES=-network-sandbox and glib-networking rdep
+	sed -i -e '/giomm_tls_client/d' tests/meson.build || die
+
+	if ! use test; then
+		sed -i -e "/^subdir('tests')/d" meson.build || die
+	fi
+}
+
+multilib_src_configure() {
+	local emesonargs=(
+		-Dwarnings=min
+		-Dbuild-deprecated-api=true
+		$(meson_native_use_bool gtk-doc build-documentation)
+		$(meson_use debug debug-refcounting)
+		-Dbuild-examples=false
+	)
+	meson_src_configure
+}

dev-cpp/glibmm/Manifest
@@ -1,3 +1,4 @@
 DIST glibmm-2.66.7.tar.xz 8773780 BLAKE2B faef7b6a9ca91aed3ac7ddeddc0413f87daba4b8d1e887431bd7db8b7cae66ec449292bbb0fb9e4e574d1ce680853ab0ffb476abd76c19257c7434389ba3d2cc SHA512 09b99622c051afd264d7a9b23025ee28ec870c6d23a5084ef7192f67b96fb8f16be5dbb2e8a4ff8a1d4ff873d3e73b4e25dee122049266687f471dcd3989b5bb
 DIST glibmm-2.66.8.tar.xz 8597344 BLAKE2B 43ce7739259b928ed71946b3de8090aab9c4c900d8233dbe16cef2a1ff9b9521f304b50bf4a57c9398aaa8deed882600814551404991d1cdd09b3c45bb461cc7 SHA512 4ebf203324d3ee95c47012915efb39d4dc59eb7a6f337e7b8c7c0b3589574b07967974363931b0d4159618f88178b04715b2c359c3dc3f67a7781bfac0d9f277
 DIST glibmm-2.78.1.tar.xz 9486784 BLAKE2B 2875d93b0cedb6bf84cb463b1c73743c55dc446ee0c1dce36344a8e0ff77cec8d890d1e3b0f1d4a841d293ab123cc60bf5c6b55ed9a0456fe235d928bf28ff05 SHA512 5ace15c492be553e2c6abd8d0699197239261feaa2b45ff77181f59bb98b584dc822bdd46dbdee35691cc5a955a3b88e03f58532459236fd780823354c35d0a6
+DIST glibmm-2.84.0.tar.xz 9596876 BLAKE2B efbf89adaed0d673bc6391f44205cdbfa5400c4137a199cb69bb7580b81ae0e2d2304906b0b15409c68b369929d44fb2b9134d23638d7244a69f92ccfc7a8402 SHA512 dbe7d7b39ccf4a4acb0a67a0eab1a4b5f47dff3c0ae645aa2eaca498b646ee2da0f6f8ef81b3253621119d94bcdc3b1d4dcbf234eedfbd20aaa065c430e67daf

commit 7084d2408c854ec84d8316e524c0c6635a03b260

dev-cpp/glibmm: restore Perl build deps
The tool seems to be run unconditionally. Not sure why but this seems
to have both been moved around and lost a few times over the years.

Bug: https://bugs.gentoo.org/788895
Closes: https://bugs.gentoo.org/978688
Fixes: 36aab3ec3df519784c40ef762b47caceb2a3b6bb
Fixes: 043ac9c3ba4aa9d10efe37b9a85c9c0cea0810cd
Signed-off-by: Sam James <sam@gentoo.org>

dev-cpp/glibmm/glibmm-2.66.8.ebuild
@@ -21,11 +21,11 @@ RDEPEND="
 DEPEND="${RDEPEND}"
 BDEPEND="
 	${PYTHON_DEPS}
+	dev-lang/perl
+	dev-perl/XML-Parser
 	virtual/pkgconfig
 	gtk-doc? (
 		app-text/doxygen[dot]
-		dev-lang/perl
-		dev-perl/XML-Parser
 		dev-libs/libxslt
 		media-gfx/graphviz
 	)

dev-cpp/glibmm/glibmm-2.84.0.ebuild
@@ -21,10 +21,11 @@ RDEPEND="
 DEPEND="${RDEPEND}"
 BDEPEND="
 	${PYTHON_DEPS}
+	dev-lang/perl
+	dev-perl/XML-Parser
 	virtual/pkgconfig
 	gtk-doc? (
 		app-text/doxygen[dot]
-		dev-lang/perl
 		dev-libs/libxslt
 	)
 "

dev-cpp/glibmm/glibmm-2.88.0.ebuild
@@ -21,10 +21,11 @@ RDEPEND="
 DEPEND="${RDEPEND}"
 BDEPEND="
 	${PYTHON_DEPS}
+	dev-lang/perl
+	dev-perl/XML-Parser
 	virtual/pkgconfig
 	gtk-doc? (
 		app-text/doxygen[dot]
-		dev-lang/perl
 		dev-libs/libxslt
 	)
 "

dev-cpp/glibmm/glibmm-2.88.1.ebuild
@@ -22,10 +22,11 @@ DEPEND="${RDEPEND}"
 BDEPEND="
 	${PYTHON_DEPS}
 	dev-cpp/mm-common
+	dev-lang/perl
+	dev-perl/XML-Parser
 	virtual/pkgconfig
 	gtk-doc? (
 		app-text/doxygen[dot]
-		dev-lang/perl
 		dev-libs/libxslt
 	)
 "