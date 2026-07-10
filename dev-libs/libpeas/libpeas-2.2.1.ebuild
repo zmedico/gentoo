zmedico/gentoo

commit 5382af8a244e711c99931f33199e32a89bd4e5f1

dev-libs/libpeas: Bump to 2.0.3
Signed-off-by: Guillermo Joandet <gjoandet@gmail.com>
Closes: https://github.com/gentoo/gentoo/pull/34835
Signed-off-by: Pacho Ramos <pacho@gentoo.org>

dev-libs/libpeas/Manifest
@@ -1,1 +1,2 @@
 DIST libpeas-1.36.0.tar.xz 195432 BLAKE2B 12b12f041784e5fbeb03a36cad62625f7b60f770f7ff83d0660b28288262e125c2ca5f83032d4ae96faaa7b815ba9dcb245d3cbdac823765435a21e1eb08f663 SHA512 254087089475a3c1415e0ba8efb17aac56b96b1eee7c8e12933e72cc551ee39e9e3eac2d3d602bf9d369dd9fc7d3f5141e7683dec11b40b6c7b961b8e6c70a11
+DIST libpeas-2.0.3.tar.xz 156608 BLAKE2B 495a089f8ed9be0589f5c1129dbe054857e6f6ad62a46348dc0c1e4ee8aa96798615d610fec93a86a954020011032628344e4bbcfe6015b8b86ac7ef5dbf9c2d SHA512 ec4e0c0ec8d4cb513a1c30f74ad6c9181eaf6e91babcfce2872715ab57f0c3c6334b18f3dbc6271d14635ccfcda52a2101f81c3417b37bb82c163be3aed93f48

dev-libs/libpeas/libpeas-2.0.3.ebuild
@@ -1,0 +1,91 @@
+# Copyright 1999-2024 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+LUA_COMPAT=( lua5-1 )
+PYTHON_COMPAT=( python3_{10..13} )
+
+inherit gnome.org lua-single meson python-single-r1 vala virtualx xdg
+
+DESCRIPTION="A GObject plugins library"
+HOMEPAGE="https://gitlab.gnome.org/GNOME/libpeas"
+
+LICENSE="LGPL-2.1+"
+SLOT="2"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~ia64 ~loong ~ppc ~ppc64 ~riscv ~sparc ~x86 ~amd64-linux ~x86-linux"
+
+IUSE="glade +gtk gtk-doc javascript lua +python vala"
+REQUIRED_USE="lua? ( ${LUA_REQUIRED_USE} )
+	python? ( ${PYTHON_REQUIRED_USE} )"
+
+RDEPEND="
+	>=dev-libs/glib-2.44:2
+	>=dev-libs/gobject-introspection-1.39:=
+	javascript? (
+		>=dev-libs/gjs-1.78.5
+		>=dev-lang/spidermonkey-115:115
+	)
+	gtk? ( >=x11-libs/gtk+-3.0.0:3[introspection] )
+	glade? ( >=dev-util/glade-3.9.1:3.10 )
+	lua? (
+		${LUA_DEPS}
+		$(lua_gen_cond_dep '
+			>=dev-lua/lgi-0.9.0[${LUA_USEDEP}]
+		')
+	)
+	python? (
+		${PYTHON_DEPS}
+		$(python_gen_cond_dep '
+			>=dev-python/pygobject-3.2:3[${PYTHON_USEDEP}]
+		')
+	)
+"
+DEPEND="${RDEPEND}"
+BDEPEND="
+	dev-util/glib-utils
+	gtk-doc? (
+		>=dev-util/gtk-doc-1.11
+		>=dev-util/gi-docgen-2021.7
+		app-text/docbook-xml-dtd:4.3
+	)
+	>=sys-devel/gettext-0.19.8
+	virtual/pkgconfig
+"
+
+pkg_setup() {
+	use lua && lua-single_pkg_setup
+	use python && python-single-r1_pkg_setup
+}
+
+src_prepare() {
+	default
+	use vala && vala_setup
+}
+
+src_configure() {
+	local emesonargs=(
+		$(meson_use javascript gjs)
+		$(meson_use lua lua51)
+		$(meson_use python python3)
+		# introspection was always enabled in autotools; would need readiness by consumers
+		# to USE flag it, but most need it for python plugins anyways
+		-Dintrospection=true
+		$(meson_use vala vapi)
+		$(meson_use gtk-doc gtk_doc)
+	)
+	meson_src_configure
+}
+
+src_test() {
+	virtx meson_src_test
+}
+
+src_install() {
+	meson_src_install
+
+	if use gtk-doc; then
+		mkdir -p "${ED}"/usr/share/gtk-doc/html/ || die
+		mv "${ED}"/usr/share/doc/libpeas{,-gtk}-1.0 "${ED}"/usr/share/gtk-doc/html/ || die
+	fi
+}

dev-libs/libpeas/metadata.xml
@@ -7,6 +7,7 @@
   </maintainer>
   <use>
     <flag name="glade">Install the glade catalog</flag>
+    <flag name="javascript">Enable JavaScript support using <pkg>dev-libs/gjs</pkg></flag>
     <flag name="python">Build support for loading <pkg>dev-lang/python</pkg> plugins</flag>
   </use>
   <upstream>

commit 7851a246b99c9f019e2cb05d0020a1a8795b20f0

dev-libs/libpeas: add 2.2.1
restore luajit and the patch for gentoo-lua
update the version requirements (pygobject-3.55 for the patch)
add missing dev-lang/vala and move vala_setup in src_configure
enable py3.15

Fixes: 5382af8a244e711c99931f33199e32a89bd4e5f1
Closes: https://bugs.gentoo.org/943122
Signed-off-by: Nicolas PARLANT <nicolas.parlant@parhuet.fr>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/1381
Signed-off-by: Sam James <sam@gentoo.org>

dev-libs/libpeas/Manifest
@@ -1,3 +1,4 @@
 DIST libpeas-1.36.0.tar.xz 195432 BLAKE2B 12b12f041784e5fbeb03a36cad62625f7b60f770f7ff83d0660b28288262e125c2ca5f83032d4ae96faaa7b815ba9dcb245d3cbdac823765435a21e1eb08f663 SHA512 254087089475a3c1415e0ba8efb17aac56b96b1eee7c8e12933e72cc551ee39e9e3eac2d3d602bf9d369dd9fc7d3f5141e7683dec11b40b6c7b961b8e6c70a11
 DIST libpeas-1.38.1.tar.xz 196456 BLAKE2B 0256f7734ad73c175cbef8c67447f26c2dec21ac1d92de9a84a744d5a8ce90ec420f8f1dd13b8d946578888e3234ba83ebbfffc31f51c5b7208e228d0d2ffd07 SHA512 5e4dc3cde9c0fdd268acefefac3b0e200b2cbd5154dc619c5665a485f22b861396ac3f243f01d621fb40c37b3c566e23c656d95d7d700b7673fa2b0168d2ae8e
 DIST libpeas-2.0.7.tar.xz 157468 BLAKE2B 4688c6cde69abb6496ab08eda0ab57c032712220aaba51d09720e10cd8df12085c517fbe6b2a2e73c85a731eeb18efc0521f598b81f2070c10b322428bf3b6f6 SHA512 105cdc7b4ec681ec7b1a2c7d52bd891908bf0ebb32ac0f04f24adcf49e73236848f8ea1324c70ebc7490d316eb439f72de23db85ef4643e3301276dfbdec912b
+DIST libpeas-2.2.1.tar.xz 157488 BLAKE2B 835cf3427b7e8eabd2a5037ddf88cf8d2d601fda70ecc4d4e05544ce99015fe8688f79233e4633de8c2a2281a3d874dd1f0f1da64b1d2f5d69a44cd503509f77 SHA512 9c7edaa39ef3e542c84417749979b478a592998c1ee3f33502fd43a53aa5fc57f006adb697b3b6893574d2c2796ef55271ac2bb90d868ecdd45621d1072252c5

dev-libs/libpeas/files/libpeas-2.2.1-lua_impl.patch
@@ -1,0 +1,79 @@
+Rebased From b693feda0148eb421e1f59d468caceb6f3183a82 Mon Sep 17 00:00:00 2001
+From: Mart Raudsepp <mart@leio.tech>
+Date: Mon, 16 Mar 2020 23:43:12 +0200
+Subject: [PATCH] build: Handle lua as needed by Gentoo
+
+Gentoo currently uses unversioned lua:0 - lua.pc instad of lua5.1.pc,
+/usr/bin/lua instead of /usr/bin/lua5.1
+Additionally lua and luajit can be installed in parallel, but only one
+should be chosen - so add explicit meson options to choose between them.
+This is currently in a hacky un-upstreamable way: working correctly only
+if both options are disabled, or only one is enabled and the other
+disabled, but having logic issues if any is "auto" or both enabled; this
+is because feature option is mainly used to handle explicit choice
+between them more easily in a Gentoo-specific patch without having to
+add various conditional branches thanks to the feature being disabled
+acting as a full disabler, so the other option is automatically not
+checked.
+--- a/meson.build
++++ b/meson.build
+@@ -144,18 +144,12 @@ if not python3_dep.found() or python_path != ''
+ endif
+ 
+ pygobject_dep = dependency('pygobject-3.0', version: pygobject_req, required: false)
+-lua51_dep = dependency('lua51', version: lua_req, required: false)
+-if not lua51_dep.found()
+-  lua51_dep = dependency('lua-5.1', version: lua_req, required: false)
+-endif
+-luajit_dep = dependency('luajit', version: luajit_req, required: false)
++lua51_dep = dependency('lua', version: lua_req, required: get_option('lua51'))
++luajit_dep = dependency('luajit', version: luajit_req, required: get_option('luajit'))
+ lua_lgi_found = false
+ lua_lgi_ver = 'not found'
+-lua51_prg = find_program('lua5.1', required: false)
+-if not lua51_prg.found()
+-  lua51_prg = find_program('lua51', required: false)
+-endif
+-luajit_prg = find_program('luajit', required: false)
++lua51_prg = find_program('lua', required: get_option('lua51'))
++luajit_prg = find_program('luajit', required: get_option('luajit'))
+ xmllint_prg = find_program('xmllint', required: false)
+ generate_vapi = get_option('vapi')
+ vapigen_prg = find_program('vapigen', required: generate_vapi)
+@@ -217,6 +211,8 @@ if lua_found
+             lua51_lgi_dep = declare_dependency(version: lua_lgi_ver)
+             lua_lgi_found = true
+         endif
++    else
++        error('lua support requested but lua-lgi not found')
+     endif
+     message('lua-lgi version: ' + lua_lgi_ver)
+ endif
+@@ -402,11 +398,7 @@ endif
+ 
+ build_gjs_loader = get_option('gjs')
+ 
+-build_lua51_loader = get_option('lua51')
+-lua51_found = (luajit_dep.found() or lua51_dep.found()) and lua_lgi_found
+-if build_lua51_loader and not lua51_found
+-  error('Lua51 requested but failed to locate suitable Lua51 and LGI support')
+-endif
++build_lua51_loader = (luajit_dep.found() or lua51_dep.found()) and lua_lgi_found
+ 
+ build_python3_loader = get_option('python3')
+ python3_found = python3_dep.found() and pygobject_dep.found()
+--- a/meson_options.txt
++++ b/meson_options.txt
+@@ -3,8 +3,11 @@ option('gjs',
+        description: 'Enable GJS support (requires gjs-1.0)')
+ 
+ option('lua51',
+-       type: 'boolean', value: true,
++       type: 'feature', value: 'enabled',
+        description: 'Enable Lua 5.1 support (requires lua-lgi)')
++option('luajit',
++       type: 'feature', value: 'disabled',
++       description: 'Use LuaJIT for Lua 5.1 support (requires lua-lgi)')
+ 
+ option('python3',
+        type: 'boolean', value: true,

dev-libs/libpeas/libpeas-2.2.1.ebuild
@@ -1,0 +1,96 @@
+# Copyright 1999-2026 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+LUA_COMPAT=( lua5-1 luajit )
+PYTHON_COMPAT=( python3_{12..15} )
+
+inherit gnome.org lua-single meson python-single-r1 vala virtualx xdg
+
+DESCRIPTION="A GObject plugins library"
+HOMEPAGE="https://gitlab.gnome.org/GNOME/libpeas"
+
+LICENSE="LGPL-2.1+"
+SLOT="2"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~loong ~ppc ~ppc64 ~riscv ~sparc ~x86"
+
+IUSE="glade gtk-doc javascript lua +python vala"
+REQUIRED_USE="
+	lua? ( ${LUA_REQUIRED_USE} )
+	python? ( ${PYTHON_REQUIRED_USE} )
+"
+
+RDEPEND="
+	>=dev-libs/glib-2.86:2
+	>=dev-libs/gobject-introspection-1.82.0-r2:=
+	javascript? (
+		>=dev-libs/gjs-1.78.5
+		dev-lang/spidermonkey:140=
+	)
+	glade? ( >=dev-util/glade-3.9.1:3.10 )
+	lua? (
+		${LUA_DEPS}
+		$(lua_gen_cond_dep '
+			>=dev-lua/lgi-0.9.0[${LUA_USEDEP}]
+		')
+	)
+	python? (
+		${PYTHON_DEPS}
+		$(python_gen_cond_dep '
+			>=dev-python/pygobject-3.55:3[${PYTHON_USEDEP}]
+		')
+	)
+"
+DEPEND="${RDEPEND}"
+BDEPEND="
+	dev-util/glib-utils
+	gtk-doc? (
+		>=dev-util/gtk-doc-1.11
+		>=dev-util/gi-docgen-2021.7
+		app-text/docbook-xml-dtd:4.3
+	)
+	vala? ( dev-lang/vala )
+	>=sys-devel/gettext-0.19.8
+	virtual/pkgconfig
+"
+
+PATCHES=(
+	# Gentoo-specific lua tweak hack
+	"${FILESDIR}"/${PN}-2.2.1-lua_impl.patch
+	"${FILESDIR}"/${PN}-1.38.1-test-extension.patch
+)
+
+pkg_setup() {
+	use lua && lua-single_pkg_setup
+	use python && python-single-r1_pkg_setup
+}
+
+src_configure() {
+	use vala && vala_setup
+	local emesonargs=(
+		$(meson_feature $(usex lua '!lua_single_target_luajit' 'lua') lua51)
+		$(meson_feature $(usex lua 'lua_single_target_luajit' 'lua') luajit)
+		$(meson_use javascript gjs)
+		$(meson_use python python3)
+		# introspection was always enabled in autotools; would need readiness by consumers
+		# to USE flag it, but most need it for python plugins anyways
+		-Dintrospection=true
+		$(meson_use vala vapi)
+		$(meson_use gtk-doc gtk_doc)
+	)
+	meson_src_configure
+}
+
+src_test() {
+	virtx meson_src_test
+}
+
+src_install() {
+	meson_src_install
+
+	if use gtk-doc; then
+		mkdir -p "${ED}"/usr/share/gtk-doc/html/ || die
+		mv "${ED}"/usr/share/doc/libpeas-2 "${ED}"/usr/share/gtk-doc/html/ || die
+	fi
+}