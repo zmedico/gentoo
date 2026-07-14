zmedico/gentoo

commit 30961de784a5430539a6b5ae88151884b59cb058

qt-utils.eclass: drop unnecessary head -n 1
My fault that this ended up there, confused this with something else.
type -P should only ever return one line even with multiple matches.

Thanks-to: James Le Cuirot <chewi@gentoo.org>
Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

eclass/qt-utils.eclass
@@ -105,8 +105,7 @@ qt_get_bindir() {
 qt_get_broot_binary() {
 	_qt_get_check_func_call "$@"
 	PATH=${BROOT}$(qt_get_libexecdir ${1}):${BROOT}$(qt_get_bindir ${1}) \
-		type -P "${2}" | head -n 1
-	pipestatus || die "${FUNCNAME[0]}: failed trying to detect binary: ${2}"
+		type -P "${2}" || die "${FUNCNAME[0]}: failed trying to detect binary: ${2}"
 }
 
 # @FUNCTION: qt_get_headerdir

commit dda55cce7621021ac89be94e3b95a4a8e7583c5c

qt-utils.eclass: drop unused inherit
Forgot about that and pkgcheck cannot pick this one up.

Fixes: 30961de784a5430539a6b5ae88151884b59cb058
Signed-off-by: Ionen Wolkens <ionen@gentoo.org>

eclass/qt-utils.eclass
@@ -18,8 +18,7 @@ if [[ -z ${_QT_UTILS_ECLASS} ]]; then
 _QT_UTILS_ECLASS=1
 
 case ${EAPI} in
-	8) inherit eapi9-pipestatus ;;
-	9) ;;
+	8|9) ;;
 	*) die "${ECLASS}: EAPI ${EAPI:-0} not supported" ;;
 esac
 