zmedico/gentoo

commit 2cf82f2bd4f963f12edaac61c58d2c80fd00073d

app-dicts/myspell-en: add 20260701
Closes: https://bugs.gentoo.org/969098
Signed-off-by: Pacho Ramos <pacho@gentoo.org>

app-dicts/myspell-en/myspell-en-20260701.ebuild
@@ -1,0 +1,62 @@
+# Copyright 1999-2026 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit myspell-r2
+
+DESCRIPTION="English dictionaries for myspell/hunspell"
+HOMEPAGE="
+	https://extensions.libreoffice.org/extensions/english-dictionaries
+	https://github.com/marcoagpinto/aoo-mozilla-en-dict
+	https://proofingtoolgui.org
+"
+SRC_URI="https://extensions.libreoffice.org/assets/downloads/41/1756695874/dict-en-20250901_lo.oxt"
+
+LICENSE="BSD MIT LGPL-3+"
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~loong ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~x64-macos"
+
+PLOCALES=( "en-AU" "en-CA" "en-GB" "en-US" "en-ZA" )
+IUSE="${PLOCALES[*]/#/+l10n_}"
+REQUIRED_USE="|| ( ${PLOCALES[*]/#/l10n_} )"
+
+src_prepare() {
+	# This thesaurus is used by all the English dictionaries, see
+	# ./dictionaries.xcu in the distfile, lines 71-81.
+	MYSPELL_THES=(
+		"th_en_US_v2.dat"
+		"th_en_US_v2.idx"
+	)
+
+	local non_us_dic_used=0
+	local mylinguas
+	for lang in "${PLOCALES[@]}"; do
+		mylinguas="${lang/-/_}"
+		if use "l10n_${lang}"; then
+			MYSPELL_DICT+=( "${mylinguas}.aff" "${mylinguas}.dic" )
+			if [[ ${lang} == en-US ]]; then
+				MYSPELL_HYPH=( "hyph_en_US.dic" )
+			else
+				non_us_dic_used=1
+			fi
+		else
+			rm "README_${mylinguas}.txt" || die
+			if [[ ${lang} == "en-US" ]]; then
+				rm "README_hyph_en_US.txt" || die
+			elif [[ ${lang} == "en-GB" ]]; then
+				rm "README_en_GB_thes.txt" || die
+			fi
+		fi
+	done
+
+	if [[ non_us_dic_used -eq 1 ]]; then
+		# This is used by every English variety, except for the en-US, see
+		# ./dictionaries.xcu, lines 60-70.
+		MYSPELL_HYPH+=( "hyph_en_GB.dic" )
+	else
+		rm "README_hyph_en_GB.txt" || die
+	fi
+
+	default
+}

commit 69070c225ddb99073500d7d8ec129e55562b23e7

app-dicts/myspell-en: add 20260701, drop USE flags
Fixes: 2cf82f2bd4f963f12edaac61c58d2c80fd00073d
Closes: https://bugs.gentoo.org/969098
Co-authored-by: Zurab Kvachadze <zurabid2016@gmail.com>
Closes: https://github.com/gentoo/gentoo/pull/38908
Signed-off-by: Alexander Kurakin <kuraga333@mail.ru>
Part-of: https://codeberg.org/gentoo/gentoo/pulls/1411
Merges: https://codeberg.org/gentoo/gentoo/pulls/1411
Signed-off-by: Pacho Ramos <pacho@gentoo.org>

app-dicts/myspell-en/Manifest
@@ -1,1 +1,2 @@
 DIST dict-en-20250901_lo.oxt 6277996 BLAKE2B 654d7d81d35541fe740432fe12dcf70e970a5e9ce8082a8ab27f76146204df7d79638a40eb962826b834309fd513da1253b48f08ee84fba44a30afa315245f94 SHA512 5eb9842da6ae6630be67ec1db026e2cc5d8acff014aa938d742ab35fc332673f17e6e3e77061af964839692e05bc3ea0ba36a066507d562f4dd9274e7ee388f6
+DIST dict-en-20260701.oxt 7525212 BLAKE2B 07576f93d94c0f86e17eec9b25990280bdab771dc9f162bbbf709eeda0cd60cf70a3364ba9551d9e5952a6d90f3aa7a8dcd0f3ba67aaccb4fa34aed372d892f3 SHA512 b4a7c724d2e64db19bd6772ebd2e0312a2709766d8255833d32325fa3fcb4c0e06e867b21a9206a022edc85654df14de486c52960a76f871f8cecc3f11f36c05

app-dicts/myspell-en/myspell-en-20260701.ebuild
@@ -1,0 +1,39 @@
+# Copyright 1999-2026 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit myspell-r2
+
+DESCRIPTION="English dictionaries for myspell/hunspell"
+HOMEPAGE="
+	https://extensions.libreoffice.org/extensions/english-dictionaries
+	https://github.com/marcoagpinto/aoo-mozilla-en-dict
+	https://proofingtoolgui.org
+"
+SRC_URI="https://extensions.libreoffice.org/assets/downloads/41/1782877663/dict-en-20260701.oxt"
+
+LICENSE="BSD MIT LGPL-3+"
+SLOT="0"
+KEYWORDS="~alpha ~amd64 ~arm ~arm64 ~hppa ~loong ~mips ~ppc ~ppc64 ~riscv ~s390 ~sparc ~x86 ~x64-macos"
+
+src_prepare() {
+	local PLOCALES=( "en_AU" "en_CA" "en_GB" "en_US" "en_GB-oxendict" "en_ZA" )
+	# This thesaurus is used by all the English dictionaries, see
+	# ./dictionaries.xcu in the distfile, lines 82-92.
+	MYSPELL_THES=(
+		"th_en_US_v2.dat"
+		"th_en_US_v2.idx"
+	)
+
+	for lang in "${PLOCALES[@]}"; do
+		MYSPELL_DICT+=( "${lang}.aff" "${lang}.dic" )
+	done
+
+	MYSPELL_HYPH=(
+		"hyph_en_US.dic"
+		"hyph_en_GB.dic"
+	)
+
+	default
+}