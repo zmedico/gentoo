zmedico/gentoo

commit 00cfb86cc65123110e8346b62184d82df503e256

media-video/ffmpeg-chromium: add 150.0.7871.12
Generated from `python3 package-chromium-ffmpeg.py 150.0.7871.12` in
chromium-tools.git. Versioned like this as we already had a `150`.

Signed-off-by: Sam James <sam@gentoo.org>

media-video/ffmpeg-chromium/Manifest
@@ -10,5 +10,6 @@ DIST ffmpeg-chromium-146.tar.xz 11133040 BLAKE2B e4c5dd4865272b09377257432e35f34
 DIST ffmpeg-chromium-147.tar.xz 11137344 BLAKE2B bf8339417bb8eed2efff2effeb702231461e9d4b22a7eaa41c929c2e44e4e42db6bdbae6d89f0726dfce623fa029c3949ce7d50d5f81bf72f84a8755f277af0a SHA512 2af6888a0c20c3717fba8aa7e813ae2675d0ce14a60b4ba3fd6dd317f4db71271daf3751eab50b7c165c829ab81b81567d602e7f79ab9e1fc5413c3731b82855
 DIST ffmpeg-chromium-148.tar.xz 11142936 BLAKE2B c332d6e02fdf40edfb287352110e5f746025ddc783b802b750badd99385ab80867ec38367f794a26395796fdddd7468a6ec06663fdddf48d0f2fe72094b59883 SHA512 d108defc08bf9b41c068714ee90cd0334e8680be20ee11142635d6c41fff889da901dbc3422a22ef014ecfceef7dccfa7cb557a765508392a522e9566baabe5e
 DIST ffmpeg-chromium-149.tar.xz 11205008 BLAKE2B 335db3d049d8619a794b344d47d2863681b53dd498b94c67b08bab66ea7bab1eb3c03e5c8f73515c538dcf49af8c7f642124076fb44ce77659d1ff5db966885b SHA512 6aa5b8d6b5f3190d7165ebddaa63142d5eaa1e0bfe1f222add6aab34e05a58c4c89c3d1d042e364eb20a19c88c560d5d2c3b858ec425d174628ba3769ff2b844
+DIST ffmpeg-chromium-150.0.7871.12.tar.xz 11330684 BLAKE2B 3c5341f3020304934daac23218dd4db50152ce4463d1572a63e1a903ed32f43226f8add835d2504370be83deb4a2b109e5049bb18b49fe3f8af0c18ba63451c7 SHA512 d57e15b0f4709f03155ca1128ec09655aa694f19d8264bec0f4b6631d3101ed2640b10960121f08ce8dd8985258295632b1bd3fa5be1adb7f57b934a3d79eb6b
 DIST ffmpeg-chromium-150.tar.xz 11206644 BLAKE2B 311dbca2958af510515b6639a00cd570e0b0395b5c0edb3df1e22e65302e24747eaff9c102488dbf309f001d137a75013cb818b1903bc8b36ecf3418602ec75c SHA512 fefb456b890c89020e0795be160f20dfb378b3353237a5ecd907b71ece548950ef29bb9352e2de6ab9ab84f3003d944af67f6bc12c0f978b5db49f6a5f0176dc
 DIST ffmpeg-chromium-151.tar.xz 11334732 BLAKE2B 2881c5264c0042575385730eaf0bae00c1065762d120f3db519d849204278f9326f5f8b262bf34cfa6f1e11cb79853bc6d5a282dfb4f2fe624bd876fe8d41e37 SHA512 51edddf7ed0618da896e3f4491434f5d0499d59696e66d580ee48244c4b979d5310cd5c78d1a8b97ab5555c4fb3b54e82d8e15b2130c06002ef1b8a2f5c2c39c

media-video/ffmpeg-chromium/ffmpeg-chromium-150.0.7871.12.ebuild
@@ -1,0 +1,241 @@
+# Copyright 1999-2026 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=9
+
+inherit flag-o-matic toolchain-funcs
+
+COMMIT=ad41607c61898cf7150e0fb20fe4bbabd44922a3
+DESCRIPTION="FFmpeg built specifically for codec support in Chromium-based browsers"
+HOMEPAGE="https://ffmpeg.org/"
+SRC_URI="https://distfiles.gentoo.org/pub/proj/chromium/ffmpeg/${P}.tar.xz"
+
+LICENSE="
+	!gpl? ( LGPL-2.1 )
+	gpl? ( GPL-2 )
+"
+SLOT="${PV}"
+
+KEYWORDS="~amd64 ~arm ~arm64 ~loong ~ppc64"
+
+# Options to use as use_enable in the foo[:bar] form.
+# This will feed configure with $(use_enable foo bar)
+# or $(use_enable foo foo) if no :bar is set.
+# foo is added to IUSE.
+FFMPEG_FLAG_MAP=(
+		cpudetection:runtime-cpudetect debug
+		+gpl
+		vaapi vdpau vulkan
+		nvenc:ffnvcodec
+		# Threads; we only support pthread for now but ffmpeg supports more
+		+threads:pthreads
+)
+
+IUSE="
+	${FFMPEG_FLAG_MAP[@]%:*}
+"
+
+# Strings for CPU features in the useflag[:configure_option] form
+# if :configure_option isn't set, it will use 'useflag' as configure option
+ARM_CPU_FEATURES=(
+	cpu_flags_arm_thumb:armv5te
+	cpu_flags_arm_v6:armv6
+	cpu_flags_arm_thumb2:armv6t2
+	cpu_flags_arm_neon:neon
+	cpu_flags_arm_vfp:vfp
+	cpu_flags_arm_vfpv3:vfpv3
+	cpu_flags_arm_v8:armv8
+	cpu_flags_arm_asimddp:dotprod
+	cpu_flags_arm_i8mm:i8mm
+)
+ARM_CPU_REQUIRED_USE="
+	arm64? ( cpu_flags_arm_v8 )
+	cpu_flags_arm_v8? ( cpu_flags_arm_vfpv3 cpu_flags_arm_neon )
+	cpu_flags_arm_neon? (
+		cpu_flags_arm_vfp
+		arm? ( cpu_flags_arm_thumb2 )
+	)
+	cpu_flags_arm_vfpv3? ( cpu_flags_arm_vfp )
+	cpu_flags_arm_thumb2? ( cpu_flags_arm_v6 )
+	cpu_flags_arm_v6? (
+		arm? ( cpu_flags_arm_thumb )
+	)
+"
+X86_CPU_FEATURES_RAW=( 3dnow:amd3dnow 3dnowext:amd3dnowext aes:aesni avx:avx avx2:avx2 fma3:fma3 fma4:fma4 mmx:mmx
+					   mmxext:mmxext sse:sse sse2:sse2 sse3:sse3 ssse3:ssse3 sse4_1:sse4 sse4_2:sse42 xop:xop )
+X86_CPU_FEATURES=( ${X86_CPU_FEATURES_RAW[@]/#/cpu_flags_x86_} )
+X86_CPU_REQUIRED_USE="
+	cpu_flags_x86_avx2? ( cpu_flags_x86_avx )
+	cpu_flags_x86_fma4? ( cpu_flags_x86_avx )
+	cpu_flags_x86_fma3? ( cpu_flags_x86_avx )
+	cpu_flags_x86_xop?  ( cpu_flags_x86_avx )
+	cpu_flags_x86_avx?  ( cpu_flags_x86_sse4_2 )
+	cpu_flags_x86_aes? ( cpu_flags_x86_sse4_2 )
+	cpu_flags_x86_sse4_2?  ( cpu_flags_x86_sse4_1 )
+	cpu_flags_x86_sse4_1?  ( cpu_flags_x86_ssse3 )
+	cpu_flags_x86_ssse3?  ( cpu_flags_x86_sse3 )
+	cpu_flags_x86_sse3?  ( cpu_flags_x86_sse2 )
+	cpu_flags_x86_sse2?  ( cpu_flags_x86_sse )
+	cpu_flags_x86_sse?  ( cpu_flags_x86_mmxext )
+	cpu_flags_x86_mmxext?  ( cpu_flags_x86_mmx )
+	cpu_flags_x86_3dnowext?  ( cpu_flags_x86_3dnow )
+	cpu_flags_x86_3dnow?  ( cpu_flags_x86_mmx )
+"
+
+CPU_FEATURES_MAP=(
+	${ARM_CPU_FEATURES[@]}
+	${X86_CPU_FEATURES[@]}
+)
+IUSE="${IUSE}
+	${CPU_FEATURES_MAP[@]%:*}"
+
+CPU_REQUIRED_USE="
+	${ARM_CPU_REQUIRED_USE}
+	${X86_CPU_REQUIRED_USE}
+"
+
+RDEPEND="
+	>=media-libs/opus-1.0.2-r2
+	vaapi? ( >=media-libs/libva-1.2.1-r1:0= )
+	nvenc? ( >=media-libs/nv-codec-headers-11.1.5.3 )
+	vdpau? ( >=x11-libs/libvdpau-0.7 )
+	vulkan? ( >=media-libs/vulkan-loader-1.3.277:= )
+"
+
+DEPEND="${RDEPEND}
+	vulkan? ( >=dev-util/vulkan-headers-1.3.277 )
+"
+BDEPEND="
+	>=dev-build/make-3.81
+	virtual/pkgconfig
+	cpu_flags_x86_mmx? ( >=dev-lang/nasm-2.13 )
+"
+
+REQUIRED_USE="
+	vulkan? ( threads )
+	${CPU_REQUIRED_USE}"
+RESTRICT="
+	test
+"
+
+PATCHES=(
+	"${FILESDIR}"/${PN}-138-configure-enable-libopus.patch
+	"${FILESDIR}"/chromium.patch
+)
+
+src_prepare() {
+	export revision=git-N-g${COMMIT:0:10}
+	default
+
+	echo 'include $(SRC_PATH)/ffbuild/libffmpeg.mak' >> Makefile || die
+}
+
+src_configure() {
+	local myconf=( )
+
+	# Bug #918997. Will probably be fixed upstream in the next release.
+	use vulkan && append-ldflags -Wl,-z,muldefs
+
+	local ffuse=( "${FFMPEG_FLAG_MAP[@]}" )
+
+	for i in "${ffuse[@]#+}" ; do
+		myconf+=( $(use_enable ${i%:*} ${i#*:}) )
+	done
+
+	# CPU features
+	for i in "${CPU_FEATURES_MAP[@]}" ; do
+		use ${i%:*} || myconf+=( --disable-${i#*:} )
+	done
+
+	# Try to get cpu type based on CFLAGS.
+	# Bug #172723
+	# We need to do this so that features of that CPU will be better used
+	# If they contain an unknown CPU it will not hurt since ffmpeg's configure
+	# will just ignore it.
+	for i in $(get-flag mcpu) $(get-flag march) ; do
+		[[ ${i} = native ]] && i="host" # bug #273421
+		if use arm64; then # 830165 - 'host' explicitly not supported on arm64
+			[[ ${i} != host ]] && myconf+=( --cpu=${i} )
+		else
+			myconf+=( --cpu=${i} )
+		fi
+		break
+	done
+
+	# LTO support, bug #566282, bug #754654, bug #772854
+	if [[ ${ABI} != x86 ]] && tc-is-lto; then
+		# Respect -flto value, e.g -flto=thin
+		local v="$(get-flag flto)"
+		[[ -n ${v} ]] && myconf+=( "--enable-lto=${v}" ) || myconf+=( "--enable-lto" )
+	fi
+	filter-lto
+
+	# Mandatory configuration
+	myconf=(
+		--disable-stripping
+		# This is only for hardcoded cflags; those are used in configure checks that may
+		# interfere with proper detections, bug #671746 and bug #645778
+		# We use optflags, so that overrides them anyway.
+		--disable-optimizations
+		--disable-libcelt # bug #664158
+		"${myconf[@]}"
+	)
+
+	# cross compile support
+	if tc-is-cross-compiler ; then
+		myconf+=( --enable-cross-compile --arch=$(tc-arch-kernel) --cross-prefix=${CHOST}- --host-cc="$(tc-getBUILD_CC)" )
+		case ${CHOST} in
+			*mingw32*)
+				myconf+=( --target-os=mingw32 )
+				;;
+			*linux*)
+				myconf+=( --target-os=linux )
+				;;
+		esac
+	fi
+
+	# Use --extra-libs if needed for LIBS
+	set -- "${S}/configure" \
+		--prefix="${EPREFIX}/usr" \
+		--libdir="${EPREFIX}/usr/$(get_libdir)" \
+		--shlibdir="${EPREFIX}/usr/$(get_libdir)" \
+		--cc="$(tc-getCC)" \
+		--cxx="$(tc-getCXX)" \
+		--ar="$(tc-getAR)" \
+		--nm="$(tc-getNM)" \
+		--strip="$(tc-getSTRIP)" \
+		--ranlib="$(tc-getRANLIB)" \
+		--pkg-config="$(tc-getPKG_CONFIG)" \
+		--optflags="${CFLAGS}" \
+		--extra-cflags="-DCHROMIUM_NO_LOGGING" \
+		--disable-all \
+		--disable-autodetect \
+		--disable-error-resilience \
+		--disable-everything \
+		--disable-faan \
+		--disable-iamf \
+		--disable-iconv \
+		--disable-network \
+		--enable-avcodec \
+		--enable-avformat \
+		--enable-avutil \
+		--enable-libopus \
+		--enable-decoder=aac,flac,h264,libopus,mp3,pcm_alaw,pcm_f32le,pcm_mulaw,pcm_s16be,pcm_s16le,pcm_s24be,pcm_s24le,pcm_s32le,pcm_u8,vorbis \
+		--enable-demuxer=aac,flac,matroska,mov,mp3,ogg,wav \
+		--enable-parser=aac,flac,h264,mpegaudio,opus,vorbis,vp9 \
+		--enable-pic \
+		--enable-static \
+		"${myconf[@]}" \
+		${EXTRA_FFMPEG_CONF}
+
+	echo "${@}"
+	"${@}" || die
+}
+
+src_compile() {
+	emake V=1 libffmpeg
+}
+
+src_install() {
+	emake V=1 DESTDIR="${D}" install-libffmpeg
+}

commit f636c5c70a9bd47c9c728a54acd868c2d918ca55

media-video/ffmpeg-chromium: add 150.0.7871.124, drop 150.0.7871.12
Fix the version and also the SLOT.

Fixes: 00cfb86cc65123110e8346b62184d82df503e256
Signed-off-by: Sam James <sam@gentoo.org>

media-video/ffmpeg-chromium/Manifest
@@ -10,6 +10,6 @@ DIST ffmpeg-chromium-146.tar.xz 11133040 BLAKE2B e4c5dd4865272b09377257432e35f34
 DIST ffmpeg-chromium-147.tar.xz 11137344 BLAKE2B bf8339417bb8eed2efff2effeb702231461e9d4b22a7eaa41c929c2e44e4e42db6bdbae6d89f0726dfce623fa029c3949ce7d50d5f81bf72f84a8755f277af0a SHA512 2af6888a0c20c3717fba8aa7e813ae2675d0ce14a60b4ba3fd6dd317f4db71271daf3751eab50b7c165c829ab81b81567d602e7f79ab9e1fc5413c3731b82855
 DIST ffmpeg-chromium-148.tar.xz 11142936 BLAKE2B c332d6e02fdf40edfb287352110e5f746025ddc783b802b750badd99385ab80867ec38367f794a26395796fdddd7468a6ec06663fdddf48d0f2fe72094b59883 SHA512 d108defc08bf9b41c068714ee90cd0334e8680be20ee11142635d6c41fff889da901dbc3422a22ef014ecfceef7dccfa7cb557a765508392a522e9566baabe5e
 DIST ffmpeg-chromium-149.tar.xz 11205008 BLAKE2B 335db3d049d8619a794b344d47d2863681b53dd498b94c67b08bab66ea7bab1eb3c03e5c8f73515c538dcf49af8c7f642124076fb44ce77659d1ff5db966885b SHA512 6aa5b8d6b5f3190d7165ebddaa63142d5eaa1e0bfe1f222add6aab34e05a58c4c89c3d1d042e364eb20a19c88c560d5d2c3b858ec425d174628ba3769ff2b844
-DIST ffmpeg-chromium-150.0.7871.12.tar.xz 11330684 BLAKE2B 3c5341f3020304934daac23218dd4db50152ce4463d1572a63e1a903ed32f43226f8add835d2504370be83deb4a2b109e5049bb18b49fe3f8af0c18ba63451c7 SHA512 d57e15b0f4709f03155ca1128ec09655aa694f19d8264bec0f4b6631d3101ed2640b10960121f08ce8dd8985258295632b1bd3fa5be1adb7f57b934a3d79eb6b
+DIST ffmpeg-chromium-150.0.7871.124.tar.xz 11330684 BLAKE2B 3c5341f3020304934daac23218dd4db50152ce4463d1572a63e1a903ed32f43226f8add835d2504370be83deb4a2b109e5049bb18b49fe3f8af0c18ba63451c7 SHA512 d57e15b0f4709f03155ca1128ec09655aa694f19d8264bec0f4b6631d3101ed2640b10960121f08ce8dd8985258295632b1bd3fa5be1adb7f57b934a3d79eb6b
 DIST ffmpeg-chromium-150.tar.xz 11206644 BLAKE2B 311dbca2958af510515b6639a00cd570e0b0395b5c0edb3df1e22e65302e24747eaff9c102488dbf309f001d137a75013cb818b1903bc8b36ecf3418602ec75c SHA512 fefb456b890c89020e0795be160f20dfb378b3353237a5ecd907b71ece548950ef29bb9352e2de6ab9ab84f3003d944af67f6bc12c0f978b5db49f6a5f0176dc
 DIST ffmpeg-chromium-151.tar.xz 11334732 BLAKE2B 2881c5264c0042575385730eaf0bae00c1065762d120f3db519d849204278f9326f5f8b262bf34cfa6f1e11cb79853bc6d5a282dfb4f2fe624bd876fe8d41e37 SHA512 51edddf7ed0618da896e3f4491434f5d0499d59696e66d580ee48244c4b979d5310cd5c78d1a8b97ab5555c4fb3b54e82d8e15b2130c06002ef1b8a2f5c2c39c

media-video/ffmpeg-chromium/ffmpeg-chromium-150.0.7871.124.ebuild
@@ -14,7 +14,7 @@ LICENSE="
 	!gpl? ( LGPL-2.1 )
 	gpl? ( GPL-2 )
 "
-SLOT="${PV}"
+SLOT="${PV%%.*}"
 
 KEYWORDS="~amd64 ~arm ~arm64 ~loong ~ppc64"
 