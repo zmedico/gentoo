zmedico/gentoo

commit dbd60bed7b6da584859f5d6f8c8c6c297b4602d4

net-analyzer/wireshark: Move setting env vars out of mycmakeargs array
While unclear if this is even necessary, at least fix USE gui build for now.

Bug: https://bugs.gentoo.org/979359
Signed-off-by: Andreas Sturmlechner <asturm@gentoo.org>

net-analyzer/wireshark/wireshark-4.6.6.ebuild
@@ -168,6 +168,13 @@ src_configure() {
 
 	python_setup
 
+	if use gui; then
+		LRELEASE=$(qt_get_broot_binary 6 lrelease)
+		MOC=$(qt_get_broot_binary 6 moc)
+		RCC=$(qt_get_broot_binary 6 rcc)
+		UIC=$(qt_get_broot_binary 6 uic)
+	fi
+
 	mycmakeargs+=(
 		-DPython3_EXECUTABLE="${PYTHON}"
 		-DCMAKE_DISABLE_FIND_PACKAGE_{Asciidoctor,DOXYGEN}=$(usex !doc)
@@ -180,10 +187,6 @@ src_configure() {
 		-DENABLE_CCACHE=OFF
 
 		$(use androiddump && use pcap && echo -DEXTCAP_ANDROIDDUMP_LIBPCAP=yes)
-		$(usex gui LRELEASE=$(qt_get_broot_binary 6 lrelease) '')
-		$(usex gui MOC=$(qt_get_broot_binary 6 moc) '')
-		$(usex gui RCC=$(qt_get_broot_binary 6 rcc) '')
-		$(usex gui UIC=$(qt_get_broot_binary 6 uic) '')
 
 		-DBUILD_androiddump=$(usex androiddump)
 		-DBUILD_capinfos=$(usex capinfos)

net-analyzer/wireshark/wireshark-4.6.7.ebuild
@@ -168,6 +168,13 @@ src_configure() {
 
 	python_setup
 
+	if use gui; then
+		LRELEASE=$(qt_get_broot_binary 6 lrelease)
+		MOC=$(qt_get_broot_binary 6 moc)
+		RCC=$(qt_get_broot_binary 6 rcc)
+		UIC=$(qt_get_broot_binary 6 uic)
+	fi
+
 	mycmakeargs+=(
 		-DPython3_EXECUTABLE="${PYTHON}"
 		-DCMAKE_DISABLE_FIND_PACKAGE_{Asciidoctor,DOXYGEN}=$(usex !doc)
@@ -180,10 +187,6 @@ src_configure() {
 		-DENABLE_CCACHE=OFF
 
 		$(use androiddump && use pcap && echo -DEXTCAP_ANDROIDDUMP_LIBPCAP=yes)
-		$(usex gui LRELEASE=$(qt_get_broot_binary 6 lrelease) '')
-		$(usex gui MOC=$(qt_get_broot_binary 6 moc) '')
-		$(usex gui RCC=$(qt_get_broot_binary 6 rcc) '')
-		$(usex gui UIC=$(qt_get_broot_binary 6 uic) '')
 
 		-DBUILD_androiddump=$(usex androiddump)
 		-DBUILD_capinfos=$(usex capinfos)

net-analyzer/wireshark/wireshark-9999.ebuild
@@ -167,6 +167,13 @@ src_configure() {
 
 	python_setup
 
+	if use gui; then
+		LRELEASE=$(qt_get_broot_binary 6 lrelease)
+		MOC=$(qt_get_broot_binary 6 moc)
+		RCC=$(qt_get_broot_binary 6 rcc)
+		UIC=$(qt_get_broot_binary 6 uic)
+	fi
+
 	mycmakeargs+=(
 		-DPython3_EXECUTABLE="${PYTHON}"
 		-DCMAKE_DISABLE_FIND_PACKAGE_{Asciidoctor,DOXYGEN}=$(usex !doc)
@@ -179,10 +186,6 @@ src_configure() {
 		-DENABLE_CCACHE=OFF
 
 		$(use androiddump && use pcap && echo -DEXTCAP_ANDROIDDUMP_LIBPCAP=yes)
-		$(usex gui LRELEASE=$(qt_get_broot_binary 6 lrelease) '')
-		$(usex gui MOC=$(qt_get_broot_binary 6 moc) '')
-		$(usex gui RCC=$(qt_get_broot_binary 6 rcc) '')
-		$(usex gui UIC=$(qt_get_broot_binary 6 uic) '')
 
 		-DBUILD_androiddump=$(usex androiddump)
 		-DBUILD_capinfos=$(usex capinfos)

commit 0e39416f3dd174fa4f0c5999f7a6eddc83f7a5d2

net-analyzer/wireshark: do not pass multiple source paths to cmake, part 2
Reject the alternative reality whereby executing

```
/usr/bin/cmake LRELEASE=/path/to/lrelease -DCMAKE_ARGS=blah /sourcedir
```

is an "environment variable". cmake does not accept "environment
variables" as command line arguments. It accepts source paths as command
line arguments, and silently (or in recent cmake, loudly) ignores
multiple source paths passed on the command line.

Hence this code produced:
```
CMake Warning:
  Ignoring extra path from command line:

   "/var/tmp/portage/net-analyzer/wireshark-4.6.6/work/wireshark-4.6.6_build/LRELEASE=/usr/lib64/qt6/bin/lrelease"

CMake Warning:
  Ignoring extra path from command line:

   "UIC=/usr/lib64/qt6/libexec/uic"
```

and clearly never worked, so does not need to be moved.

Moving it would be broken if it did do something, since it is not
exported so cmake would no longer be able to see it.

Bug: https://bugs.gentoo.org/979359
Fixes: dbd60bed7b6da584859f5d6f8c8c6c297b4602d4
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

net-analyzer/wireshark/wireshark-4.6.6.ebuild
@@ -168,13 +168,6 @@ src_configure() {
 
 	python_setup
 
-	if use gui; then
-		LRELEASE=$(qt_get_broot_binary 6 lrelease)
-		MOC=$(qt_get_broot_binary 6 moc)
-		RCC=$(qt_get_broot_binary 6 rcc)
-		UIC=$(qt_get_broot_binary 6 uic)
-	fi
-
 	mycmakeargs+=(
 		-DPython3_EXECUTABLE="${PYTHON}"
 		-DCMAKE_DISABLE_FIND_PACKAGE_{Asciidoctor,DOXYGEN}=$(usex !doc)

net-analyzer/wireshark/wireshark-4.6.7.ebuild
@@ -168,13 +168,6 @@ src_configure() {
 
 	python_setup
 
-	if use gui; then
-		LRELEASE=$(qt_get_broot_binary 6 lrelease)
-		MOC=$(qt_get_broot_binary 6 moc)
-		RCC=$(qt_get_broot_binary 6 rcc)
-		UIC=$(qt_get_broot_binary 6 uic)
-	fi
-
 	mycmakeargs+=(
 		-DPython3_EXECUTABLE="${PYTHON}"
 		-DCMAKE_DISABLE_FIND_PACKAGE_{Asciidoctor,DOXYGEN}=$(usex !doc)

net-analyzer/wireshark/wireshark-9999.ebuild
@@ -167,13 +167,6 @@ src_configure() {
 
 	python_setup
 
-	if use gui; then
-		LRELEASE=$(qt_get_broot_binary 6 lrelease)
-		MOC=$(qt_get_broot_binary 6 moc)
-		RCC=$(qt_get_broot_binary 6 rcc)
-		UIC=$(qt_get_broot_binary 6 uic)
-	fi
-
 	mycmakeargs+=(
 		-DPython3_EXECUTABLE="${PYTHON}"
 		-DCMAKE_DISABLE_FIND_PACKAGE_{Asciidoctor,DOXYGEN}=$(usex !doc)